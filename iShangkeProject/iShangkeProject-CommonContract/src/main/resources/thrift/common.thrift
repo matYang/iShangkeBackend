56f720b0dabb803c9961d56aaf63c169e80a2318
poiz <haixinv@qq.com>
2014-07-10 00:38:54 +0800

    DataAccess query demo. Thrift service demo.

---
+++ iShangkeProject/iShangkeProject-CommonContract/src/main/resources/thrift/common.thrift
@@ -0,0 +1,10 @@
+namespace java com.ishangke.edunav.commoncontract.model
+
+/**
+ * ResponseBo
+ */
+struct ResponseBo {
+  1: i32 status
+  2: string messageKey
+}
+
--- iShangkeProject/iShangkeProject-Dataaccess/src/main/java/com/ishangke/edunav/dataaccess/model/CourseEntityExt.java
+++ iShangkeProject/iShangkeProject-Dataaccess/src/main/java/com/ishangke/edunav/dataaccess/model/CourseEntityExt.java
@@ -1,7 +1,17 @@
 package com.ishangke.edunav.dataaccess.model;
 
+import java.util.List;
+
 import com.ishangke.edunav.dataaccess.model.gen.CourseEntity;
 
 public class CourseEntityExt extends CourseEntity {
+    private List<CourseTeacherEntityExt> teacherList;
 
+    public List<CourseTeacherEntityExt> getTeacherList() {
+        return teacherList;
+    }
+
+    public void setTeacherList(List<CourseTeacherEntityExt> teacherList) {
+        this.teacherList = teacherList;
+    }
 }
