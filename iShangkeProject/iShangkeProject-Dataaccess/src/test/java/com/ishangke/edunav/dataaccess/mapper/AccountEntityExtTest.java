481f9ee8f9cb14d29e68de64d46253700aa1c43d
Aaron <467517168@qq.com>
2014-07-10 15:33:58 +0800

    modify model design

--- iShangkeProject/iShangkeProject-Dataaccess/src/test/java/com/ishangke/edunav/dataaccess/mapper/AccountEntityExtTest.java
+++ iShangkeProject/iShangkeProject-Dataaccess/src/test/java/com/ishangke/edunav/dataaccess/mapper/AccountEntityExtTest.java
@@ -1,13 +1,11 @@
 package com.ishangke.edunav.dataaccess.mapper;
 
-//import org.junit.Assert;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.transaction.annotation.Transactional;
+import org.junit.runner.RunWith;
+import org.springframework.transaction.annotation.Transactional;
 
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(locations = { "classpath*:applicationContext-dataaccessUT.xml" })
-//@Transactional
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath*:applicationContext-dataaccessUT.xml" })
+@Transactional
 public class AccountEntityExtTest {
 //    @Test
 //    public void test() {
