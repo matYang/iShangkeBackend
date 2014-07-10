73b3d79a27df63171a6b1e7eace5b22e1e29f21f
poiz <haixinv@qq.com>
2014-07-10 23:20:39 +0800

    Couse and Teacher casscading demo.

--- iShangkeProject/iShangkeProject-Dataaccess/src/main/java/com/ishangke/edunav/dataaccess/mapper/CourseEntityExtMapper.java
+++ iShangkeProject/iShangkeProject-Dataaccess/src/main/java/com/ishangke/edunav/dataaccess/mapper/CourseEntityExtMapper.java
@@ -1,10 +1,16 @@
 package com.ishangke.edunav.dataaccess.mapper;
 
+import java.util.List;
+
 import com.ishangke.edunav.dataaccess.model.CourseEntityExt;
 
 public interface CourseEntityExtMapper {
 
     public int add(CourseEntityExt courseEntityExt);
+
     public void deleteById(int id);
+
     public void update(CourseEntityExt courseEntityExt);
+
+    public List<CourseEntityExt> list(CourseEntityExt courseEntityExt);
 }
