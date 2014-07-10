3b7466390d8cc514479fa37b571583b003db885e
Aaron <467517168@qq.com>
2014-07-10 17:38:12 +0800

    ready of ut！

--- iShangkeProject/iShangkeProject-Dataaccess/src/test/java/com/ishangke/edunav/dataaccess/mapper/AccountEntityExtTest.java
+++ iShangkeProject/iShangkeProject-Dataaccess/src/test/java/com/ishangke/edunav/dataaccess/mapper/AccountEntityExtTest.java
@@ -1,14 +1,18 @@
 package com.ishangke.edunav.dataaccess.mapper;
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath*:applicationContext-dataaccessUT.xml" })
 @Transactional
 public class AccountEntityExtTest {
-//    @Test
-//    public void test() {
-//        Assert.assertTrue(true);
-//    }
+    @Test
+    public void test() {
+        Assert.assertTrue(true);
+    }
 }
