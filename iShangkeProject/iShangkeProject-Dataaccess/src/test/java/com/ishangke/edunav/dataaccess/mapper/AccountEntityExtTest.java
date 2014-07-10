761121e9be2bb9df1211b29e30553e891879fa8d
liqing0216 <1670990633@qq.com>
2014-07-10 13:48:07 +0800

    大坑

---
+++ iShangkeProject/iShangkeProject-Dataaccess/src/test/java/com/ishangke/edunav/dataaccess/mapper/AccountEntityExtTest.java
@@ -0,0 +1,16 @@
+package com.ishangke.edunav.dataaccess.mapper;
+
+//import org.junit.Assert;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.transaction.annotation.Transactional;
+
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations = { "classpath*:applicationContext-dataaccessUT.xml" })
+//@Transactional
+public class AccountEntityExtTest {
+//    @Test
+//    public void test() {
+//        Assert.assertTrue(true);
+//    }
+}
