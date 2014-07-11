32cf973da02400011e3f827d57b7e73df812a727
Harry Xiong <xiongchuhanplace@hotmail.com>
2014-07-11 12:55:28 +0800

    sdfdsf

--- iShangkeProject/iShangkeProject-Dataaccess/src/test/java/com/ishangke/edunav/dataaccess/mapper/AccountEntityExtTest.java
+++ iShangkeProject/iShangkeProject-Dataaccess/src/test/java/com/ishangke/edunav/dataaccess/mapper/AccountEntityExtTest.java
@@ -33,8 +33,7 @@ public class AccountEntityExtTest {
         userEntityExt.setDeleted(0);
         userEntityExtMapper.add(userEntityExt);
         AccountEntityExt accountEntityExt = new AccountEntityExt();
-        accountEntityExt.setId(userEntityExt.getId());
-        //TODO Chinese doesn't work 
+        accountEntityExt.setId(userEntityExt.getId());   
         accountEntityExt.setRealName("中文测试");
         accountEntityExt.setBalance(12.12);
         accountEntityExt.setBalanceEnd(22.2);
