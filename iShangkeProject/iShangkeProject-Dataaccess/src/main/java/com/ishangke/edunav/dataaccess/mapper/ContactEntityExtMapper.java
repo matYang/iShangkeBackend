6a231e739a1cbbf450e3bb07b6f6c402bfc6248c
matYang <uwse@me.com>
2014-07-11 21:48:33 +0800

    adding contact

---
+++ iShangkeProject/iShangkeProject-Dataaccess/src/main/java/com/ishangke/edunav/dataaccess/mapper/ContactEntityExtMapper.java
@@ -0,0 +1,24 @@
+package com.ishangke.edunav.dataaccess.mapper;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import com.ishangke.edunav.dataaccess.common.PaginationEntity;
+import com.ishangke.edunav.dataaccess.model.ContactEntityExt;
+
+public interface ContactEntityExtMapper {
+
+    public int add(ContactEntityExt contactEntityExt);
+
+    public void deleteById(int id);
+
+    public void update(ContactEntityExt contactEntityExt);
+
+    public int getCount();
+
+    public List<ContactEntityExt> list(
+            @Param("entity") ContactEntityExt contactEntityExt,
+            @Param("page") PaginationEntity page);
+
+    public int getListCount(@Param("entity") ContactEntityExt contactEntityExt,
+            @Param("page") PaginationEntity page);
+}
