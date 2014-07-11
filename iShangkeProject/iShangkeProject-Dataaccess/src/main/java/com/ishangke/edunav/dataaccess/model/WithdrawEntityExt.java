a25f27e89df7b930c2fffe748b2af37778be2b98
Aaron <467517168@qq.com>
2014-07-11 17:18:43 +0800

    junit for witheraw, set pool num 50

--- iShangkeProject/iShangkeProject-Dataaccess/src/main/java/com/ishangke/edunav/dataaccess/model/WithdrawEntityExt.java
+++ iShangkeProject/iShangkeProject-Dataaccess/src/main/java/com/ishangke/edunav/dataaccess/model/WithdrawEntityExt.java
@@ -1,7 +1,58 @@
 package com.ishangke.edunav.dataaccess.model;
 
+import java.util.Date;
+import java.util.Set;
+
 import com.ishangke.edunav.dataaccess.model.gen.WithdrawEntity;
 
 public class WithdrawEntityExt extends WithdrawEntity {
-
+    private int idStart;
+    private int idEnd;
+    private Set<Integer> idSet;
+    private Date lastModifyNameStart;
+    private Date lastModifyNameEnd;
+    private Date createTimeStart;
+    private Date createTimeEnd;
+    public int getIdStart() {
+        return idStart;
+    }
+    public void setIdStart(int idStart) {
+        this.idStart = idStart;
+    }
+    public int getIdEnd() {
+        return idEnd;
+    }
+    public void setIdEnd(int idEnd) {
+        this.idEnd = idEnd;
+    }
+    public Set<Integer> getIdSet() {
+        return idSet;
+    }
+    public void setIdSet(Set<Integer> idSet) {
+        this.idSet = idSet;
+    }
+    public Date getLastModifyNameStart() {
+        return lastModifyNameStart;
+    }
+    public void setLastModifyNameStart(Date lastModifyNameStart) {
+        this.lastModifyNameStart = lastModifyNameStart;
+    }
+    public Date getLastModifyNameEnd() {
+        return lastModifyNameEnd;
+    }
+    public void setLastModifyNameEnd(Date lastModifyNameEnd) {
+        this.lastModifyNameEnd = lastModifyNameEnd;
+    }
+    public Date getCreateTimeStart() {
+        return createTimeStart;
+    }
+    public void setCreateTimeStart(Date createTimeStart) {
+        this.createTimeStart = createTimeStart;
+    }
+    public Date getCreateTimeEnd() {
+        return createTimeEnd;
+    }
+    public void setCreateTimeEnd(Date createTimeEnd) {
+        this.createTimeEnd = createTimeEnd;
+    }
 }
