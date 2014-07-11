a2f000063f1b0c3c54ee06988cb965e457c35a3c
Harry Xiong <xiongchuhanplace@hotmail.com>
2014-07-11 16:44:08 +0800

    groups list xml

--- iShangkeProject/iShangkeProject-Dataaccess/src/main/java/com/ishangke/edunav/dataaccess/model/GroupEntityExt.java
+++ iShangkeProject/iShangkeProject-Dataaccess/src/main/java/com/ishangke/edunav/dataaccess/model/GroupEntityExt.java
@@ -1,7 +1,80 @@
 package com.ishangke.edunav.dataaccess.model;
 
+import java.util.Date;
+
 import com.ishangke.edunav.dataaccess.model.gen.GroupEntity;
 
 public class GroupEntityExt extends GroupEntity {
 
+    private int idStart;
+    private int idEnd;
+    private int roleIdStart;
+    private int roleIdEnd;
+    private int partnerIdStart;
+    private int partnerIdEnd;
+    private Date lastModifyTimeStart;
+    private Date lastModifyTimeEnd;
+    private Date createTimeStart;
+    private Date createTimeEnd;
+    
+    public int getIdStart() {
+        return idStart;
+    }
+    public void setIdStart(int idStart) {
+        this.idStart = idStart;
+    }
+    public int getIdEnd() {
+        return idEnd;
+    }
+    public void setIdEnd(int idEnd) {
+        this.idEnd = idEnd;
+    }
+    public int getRoleIdStart() {
+        return roleIdStart;
+    }
+    public void setRoleIdStart(int roleIdStart) {
+        this.roleIdStart = roleIdStart;
+    }
+    public int getRoleIdEnd() {
+        return roleIdEnd;
+    }
+    public void setRoleIdEnd(int roleIdEnd) {
+        this.roleIdEnd = roleIdEnd;
+    }
+    public int getPartnerIdStart() {
+        return partnerIdStart;
+    }
+    public void setPartnerIdStart(int partnerIdStart) {
+        this.partnerIdStart = partnerIdStart;
+    }
+    public int getPartnerIdEnd() {
+        return partnerIdEnd;
+    }
+    public void setPartnerIdEnd(int partnerIdEnd) {
+        this.partnerIdEnd = partnerIdEnd;
+    }
+    public Date getLastModifyTimeStart() {
+        return lastModifyTimeStart;
+    }
+    public void setLastModifyTimeStart(Date lastModifyTimeStart) {
+        this.lastModifyTimeStart = lastModifyTimeStart;
+    }
+    public Date getLastModifyTimeEnd() {
+        return lastModifyTimeEnd;
+    }
+    public void setLastModifyTimeEnd(Date lastModifyTimeEnd) {
+        this.lastModifyTimeEnd = lastModifyTimeEnd;
+    }
+    public Date getCreateTimeStart() {
+        return createTimeStart;
+    }
+    public void setCreateTimeStart(Date createTimeStart) {
+        this.createTimeStart = createTimeStart;
+    }
+    public Date getCreateTimeEnd() {
+        return createTimeEnd;
+    }
+    public void setCreateTimeEnd(Date createTimeEnd) {
+        this.createTimeEnd = createTimeEnd;
+    }
 }
