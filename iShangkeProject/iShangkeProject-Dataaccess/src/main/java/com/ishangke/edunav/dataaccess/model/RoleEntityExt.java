43bbb3b0c68ea1ab9b3030be151490f2976b846e
Harry Xiong <xiongchuhanplace@hotmail.com>
2014-07-11 16:01:54 +0800

    roles select list

--- iShangkeProject/iShangkeProject-Dataaccess/src/main/java/com/ishangke/edunav/dataaccess/model/RoleEntityExt.java
+++ iShangkeProject/iShangkeProject-Dataaccess/src/main/java/com/ishangke/edunav/dataaccess/model/RoleEntityExt.java
@@ -1,7 +1,14 @@
 package com.ishangke.edunav.dataaccess.model;
 
+import java.util.Date;
+
 import com.ishangke.edunav.dataaccess.model.gen.RoleEntity;
 
 public class RoleEntityExt extends RoleEntity {
-
+    private int idStart;
+    private int idEnd;
+    private Date lastModifyTimeStart;
+    private Date lastModifyTimeEnd;
+    private Date createTimeStart;
+    private Date createTimeEnd;
 }
