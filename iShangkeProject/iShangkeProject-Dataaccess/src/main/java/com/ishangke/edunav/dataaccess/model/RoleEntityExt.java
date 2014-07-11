079473364fcef92b1301e918b30a4a94fb8d06d3
Harry Xiong <xiongchuhanplace@hotmail.com>
2014-07-11 16:03:03 +0800

    睿哥哥别坑我了。。。

--- iShangkeProject/iShangkeProject-Dataaccess/src/main/java/com/ishangke/edunav/dataaccess/model/RoleEntityExt.java
+++ iShangkeProject/iShangkeProject-Dataaccess/src/main/java/com/ishangke/edunav/dataaccess/model/RoleEntityExt.java
@@ -11,4 +11,41 @@ public class RoleEntityExt extends RoleEntity {
     private Date lastModifyTimeEnd;
     private Date createTimeStart;
     private Date createTimeEnd;
+    
+    public int getIdStart() {
+        return idStart;
+    }
+    public void setIdStart(int idStart) {
+        this.idStart = idStart;
+    }
+    public int getIdEnd() {
+        return idEnd;
+    }
+    public void setIdEnd(int idEnd) {
+        this.idEnd = idEnd;
+    }
+    public Date getLastModifyTimeStart() {
+        return lastModifyTimeStart;
+    }
+    public void setLastModifyTimeStart(Date lastModifyTimeStart) {
+        this.lastModifyTimeStart = lastModifyTimeStart;
+    }
+    public Date getLastModifyTimeEnd() {
+        return lastModifyTimeEnd;
+    }
+    public void setLastModifyTimeEnd(Date lastModifyTimeEnd) {
+        this.lastModifyTimeEnd = lastModifyTimeEnd;
+    }
+    public Date getCreateTimeStart() {
+        return createTimeStart;
+    }
+    public void setCreateTimeStart(Date createTimeStart) {
+        this.createTimeStart = createTimeStart;
+    }
+    public Date getCreateTimeEnd() {
+        return createTimeEnd;
+    }
+    public void setCreateTimeEnd(Date createTimeEnd) {
+        this.createTimeEnd = createTimeEnd;
+    }
 }
