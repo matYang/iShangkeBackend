1d478e9a44c272632ad75072699d3ebd72f9d117
poiz <haixinv@qq.com>
2014-07-13 00:29:32 +0800

    Demo for SpringMVC RESTful client. Please check iShangkeProject-UserWeb. It runs in run-jetty-run successfully.

---
+++ iShangkeProject/iShangkeProject-UserWeb/src/main/java/com/ishangke/edunav/web/user/controller/WelcomeController.java
@@ -0,0 +1,35 @@
+package com.ishangke.edunav.web.user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ishangke.edunav.web.user.common.JsonResponse;
+import com.ishangke.edunav.web.user.vo.QueryVo;
+
+@Controller
+@RequestMapping("/api/v2/welcome")
+public class WelcomeController extends AbstractController {
+
+    @RequestMapping(value = "", method = RequestMethod.POST, produces = "application/json")
+    public @ResponseBody JsonResponse<String> welcomePost(@RequestBody QueryVo queryVo) {
+        String name = queryVo.getName();
+        JsonResponse<String> result = new JsonResponse<>();
+        result.setMessage("success");
+        result.setStatus(0);
+        result.setResponse("Welcome, " + name + "! [POST]");
+        return result;
+    }
+
+    @RequestMapping(value = "/{name}", method = RequestMethod.GET, produces = "application/json")
+    public @ResponseBody JsonResponse<String> welcomeGet(@PathVariable("name") String name) {
+        JsonResponse<String> result = new JsonResponse<>();
+        result.setMessage("success");
+        result.setStatus(0);
+        result.setResponse("Welcome, " + name + "! [GET]");
+        return result;
+    }
+}
