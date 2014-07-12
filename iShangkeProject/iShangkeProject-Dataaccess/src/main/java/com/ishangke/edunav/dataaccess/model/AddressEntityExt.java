535055be2ff5ec45717e90addb46b936c3c2570a
matYang <uwse@me.com>
2014-07-12 21:00:32 +0800

    致清哥：这里可能有火坑有天坑请看准了跳过去   adding AddressMapperXml and ContactMapperXml, passed mvm clean install

--- iShangkeProject/iShangkeProject-Dataaccess/src/main/java/com/ishangke/edunav/dataaccess/model/AddressEntityExt.java
+++ iShangkeProject/iShangkeProject-Dataaccess/src/main/java/com/ishangke/edunav/dataaccess/model/AddressEntityExt.java
@@ -1,7 +1,21 @@
 package com.ishangke.edunav.dataaccess.model;
 
 import com.ishangke.edunav.dataaccess.model.gen.AddressEntity;
+import java.util.Date;
 
 public class AddressEntityExt extends AddressEntity {
+	private int idStart;
+    private int idEnd;
 
+    private int partnerIdStart;
+    private int partnerIdEnd;
+    private int locationIdStart;
+    private int locationIdEnd;
+    private int circleIdStart;
+    private int circleIdEnd;
+
+    private Date lastModifyTimeStart;
+    private Date lastModifyTimeEnd;
+    private Date createTimeStart;
+    private Date createTimeEnd;
 }
