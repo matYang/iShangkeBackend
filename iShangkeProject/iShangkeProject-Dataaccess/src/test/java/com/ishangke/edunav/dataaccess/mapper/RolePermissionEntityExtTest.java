7f323a7901b86973b1c9a292cb8e5bfeb6c6e72f
Harry Xiong <xiongchuhanplace@hotmail.com>
2014-07-13 16:53:42 +0800

    role Permission test

--- iShangkeProject/iShangkeProject-Dataaccess/src/test/java/com/ishangke/edunav/dataaccess/mapper/RolePermissionEntityExtTest.java
+++ iShangkeProject/iShangkeProject-Dataaccess/src/test/java/com/ishangke/edunav/dataaccess/mapper/RolePermissionEntityExtTest.java
@@ -1,5 +1,84 @@
 package com.ishangke.edunav.dataaccess.mapper;
 
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ishangke.edunav.dataaccess.common.PaginationEntity;
+import com.ishangke.edunav.dataaccess.model.RolePermissionEntityExt;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath*:applicationContext-dataaccessUT.xml" })
+@Transactional
+
 public class RolePermissionEntityExtTest {
 
+    @Autowired
+    private RolePermissionEntityExtMapper rolePermissionEntityExtMapper;
+
+    @Test
+    public void testAdd(){
+        RolePermissionEntityExt rolePermissionEntityExt = new RolePermissionEntityExt();
+        rolePermissionEntityExt.setLastModifyTime(new Date());
+        rolePermissionEntityExt.setDeleted(0);
+        rolePermissionEntityExt.setRoleId(1);
+        rolePermissionEntityExt.setPermissionId(1);
+        int oldCount = rolePermissionEntityExtMapper.getCount();
+        rolePermissionEntityExtMapper.add(rolePermissionEntityExt);
+        Assert.assertSame(rolePermissionEntityExtMapper.getCount(),oldCount + 1);
+    }
+
+    @Test
+    public void testDelete(){
+        RolePermissionEntityExt rolePermissionEntityExt = new RolePermissionEntityExt();
+        rolePermissionEntityExt.setLastModifyTime(new Date());
+        rolePermissionEntityExt.setDeleted(0);
+        rolePermissionEntityExt.setRoleId(1);
+        rolePermissionEntityExt.setPermissionId(1);        
+        rolePermissionEntityExtMapper.add(rolePermissionEntityExt);
+        int oldCount = rolePermissionEntityExtMapper.getCount();
+        rolePermissionEntityExtMapper.deleteById(rolePermissionEntityExt.getId());
+        Assert.assertSame(rolePermissionEntityExtMapper.getCount(),oldCount - 1);
+    }
+
+    @Test
+    public void testList(){
+        RolePermissionEntityExt rolePermissionEntityExt = new RolePermissionEntityExt();
+        rolePermissionEntityExt.setLastModifyTime(new Date());
+        rolePermissionEntityExt.setDeleted(0);
+        rolePermissionEntityExt.setRoleId(1);
+        rolePermissionEntityExt.setPermissionId(1);        
+        rolePermissionEntityExtMapper.add(rolePermissionEntityExt);
+
+        rolePermissionEntityExt.setLastModifyTimeStart(new Date(System.currentTimeMillis() - 10000));
+
+        List<RolePermissionEntityExt> list = rolePermissionEntityExtMapper.list(rolePermissionEntityExt, null);
+        Assert.assertSame(list.size(),1);
+
+        int listSize = rolePermissionEntityExtMapper.getListCount(rolePermissionEntityExt, null);
+        Assert.assertSame(listSize,1);
+
+        PaginationEntity page = new PaginationEntity();
+        page.setOffset(0);
+        page.setSize(1);
+        
+        list = rolePermissionEntityExtMapper.list(rolePermissionEntityExt, page);
+        Assert.assertSame(list.size(),1);
+
+        listSize = rolePermissionEntityExtMapper.getListCount(rolePermissionEntityExt, page);
+        Assert.assertSame(listSize,1);
+
+    }
+    
+    @Test
+    public void testUpdate(){
+        
+    }
 }
