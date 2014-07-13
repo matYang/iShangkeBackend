9e62e9280d4baceb60769782d87a494fc5cd5af8
Harry Xiong <xiongchuhanplace@hotmail.com>
2014-07-13 16:19:42 +0800

    CouponHistory test

--- iShangkeProject/iShangkeProject-Dataaccess/src/test/java/com/ishangke/edunav/dataaccess/mapper/CouponHistoryEntityExtTest.java
+++ iShangkeProject/iShangkeProject-Dataaccess/src/test/java/com/ishangke/edunav/dataaccess/mapper/CouponHistoryEntityExtTest.java
@@ -1,6 +1,9 @@
 package com.ishangke.edunav.dataaccess.mapper;
 
+import static org.junit.Assert.fail;
+
 import java.util.Date;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -10,6 +13,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ishangke.edunav.dataaccess.common.PaginationEntity;
 import com.ishangke.edunav.dataaccess.model.CouponHistoryEntityExt;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -26,7 +30,7 @@ public class CouponHistoryEntityExtTest {
         couponHistoryEntityExt.setLastModifyTime(new Date());
         couponHistoryEntityExt.setCouponId(1);
         couponHistoryEntityExt.setDeleted(0);
-       couponHistoryEntityExtMapper.add(couponHistoryEntityExt);
+        couponHistoryEntityExtMapper.add(couponHistoryEntityExt);
         int oldcount = couponHistoryEntityExtMapper.getCount();
         couponHistoryEntityExtMapper.add(couponHistoryEntityExt);
         Assert.assertSame(couponHistoryEntityExtMapper.getCount(), oldcount + 1);
@@ -43,6 +47,61 @@ public class CouponHistoryEntityExtTest {
         int oldcount = couponHistoryEntityExtMapper.getCount();
         couponHistoryEntityExtMapper.deleteById(couponHistoryEntityExt.getId());
         Assert.assertSame(couponHistoryEntityExtMapper.getCount(), oldcount - 1);
+    }
+
+    @Test
+    public void testList(){
+        CouponHistoryEntityExt couponHistoryEntityExt = new CouponHistoryEntityExt();
+        couponHistoryEntityExt.setCreateTime(new Date());
+        couponHistoryEntityExt.setLastModifyTime(new Date());
+        couponHistoryEntityExt.setCouponId(1);
+        couponHistoryEntityExt.setDeleted(0);
+        couponHistoryEntityExtMapper.add(couponHistoryEntityExt);
+
+        couponHistoryEntityExt.setCreateTimeStart(new Date(System.currentTimeMillis() - 10000));
+        couponHistoryEntityExt.setLastModifyTimeStart(new Date(System.currentTimeMillis() - 10000));
+        
+        List<CouponHistoryEntityExt> list = couponHistoryEntityExtMapper.list(couponHistoryEntityExt, null);
+        Assert.assertSame(list.size(),1);
+
+        int listSize = couponHistoryEntityExtMapper.getListCount(couponHistoryEntityExt, null);
+        Assert.assertSame(listSize,1);
+
+        PaginationEntity page = new PaginationEntity();
+        page.setOffset(0);
+        page.setSize(1);
+        
+        list = couponHistoryEntityExtMapper.list(couponHistoryEntityExt, page);
+        Assert.assertSame(list.size(),1);
+
+        listSize = couponHistoryEntityExtMapper.getListCount(couponHistoryEntityExt, page);
+        Assert.assertSame(listSize,1);
+
+    }
+    
+    @Test
+    public void testUpdate(){
+        CouponHistoryEntityExt couponHistoryEntityExt = new CouponHistoryEntityExt();
+        couponHistoryEntityExt.setCreateTime(new Date());
+        couponHistoryEntityExt.setLastModifyTime(new Date());
+        couponHistoryEntityExt.setCouponId(1);
+        couponHistoryEntityExt.setDeleted(0);
+        couponHistoryEntityExtMapper.add(couponHistoryEntityExt);
+
+        couponHistoryEntityExt.setCreateTimeStart(new Date(System.currentTimeMillis() - 10000));
+        couponHistoryEntityExt.setLastModifyTimeStart(new Date(System.currentTimeMillis() - 10000));
+        
+        List<CouponHistoryEntityExt> list = couponHistoryEntityExtMapper.list(couponHistoryEntityExt, null);
+        Double charge = 1.2;
+        list.get(0).setCharge(charge);
+        
+        couponHistoryEntityExtMapper.update(list.get(0));
+        list = couponHistoryEntityExtMapper.list(list.get(0), null);
+        
+        if(list.get(0).getCharge().equals(charge)){
+            //Passed;
+        }else fail();
+        
     }
 
 }
