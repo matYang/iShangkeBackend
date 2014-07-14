8254c6c39f0217c65b750a6de787448d36470e06
Aaron <467517168@qq.com>
2014-07-14 16:32:44 +0800

    thrift service： 我想起那天夕阳下的奔跑，那是我逝去的青春。。。

--- iShangkeProject/iShangkeProject-CommonContract/src/main/resources/thrift/course.thrift
+++ iShangkeProject/iShangkeProject-CommonContract/src/main/resources/thrift/course.thrift
@@ -12,3 +12,28 @@ struct CourseTemplateBo {
     1: i32 id
     2: string name
 }
+
+struct CommentCourseApproveBo {
+    1: i32 id
+    2: string comment
+}
+
+struct CommentCourseTemplateApproveBo {
+    1: i32 id
+    2: string comment
+}
+
+struct CommentCourseRejectBo {
+    1: i32 id
+    2: string comment
+}
+
+struct CommentCourseTemplateRejectBo {
+    1: i32 id
+    2: string comment
+}
+
+struct CommentCourseBo {
+    1: i32 id
+    2: string commnet 
+}
