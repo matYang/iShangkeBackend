5e9886a4c98aca6d8814796c1c741984dd8bb935
Aaron <467517168@qq.com>
2014-07-16 23:22:45 +0800

    一入IT深似海

--- iShangkeProject/iShangkeProject-Service/src/main/java/com/ishangke/edunav/manager/converter/AddressConverter.java
+++ iShangkeProject/iShangkeProject-Service/src/main/java/com/ishangke/edunav/manager/converter/AddressConverter.java
@@ -1,14 +1,16 @@
-//package com.ishangke.edunav.manager.converter;
-//
-//import com.ishangke.edunav.commoncontract.model.AddressBo;
-//import com.ishangke.edunav.dataaccess.model.AddressEntityExt;
-//
-//public class AddressConverter {
-//    public static AddressBo toBo(AddressEntityExt e) {
-//        return new AddressBo();
-//    }
-//
-//    public static AddressEntityExt fromBo(AddressBo bo) {
-//        return new AddressEntityExt();
-//    }
-//}
+package com.ishangke.edunav.manager.converter;
+
+import com.ishangke.edunav.commoncontract.model.AddressBo;
+import com.ishangke.edunav.dataaccess.model.AddressEntityExt;
+
+public class AddressConverter {
+   public static AddressBo toBo(AddressEntityExt e) {
+       AddressBo addressBo = new AddressBo();
+       return addressBo;
+   }
+
+   public static AddressEntityExt fromBo(AddressBo bo) {
+       AddressEntityExt addressEntityExt = new AddressEntityExt();
+       return addressEntityExt;
+   }
+}
