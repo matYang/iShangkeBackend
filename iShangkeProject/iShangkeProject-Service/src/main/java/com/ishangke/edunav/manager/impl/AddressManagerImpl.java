b65f0217a64bd84f3b3475dbd353752b2c6e48ba
Harry Xiong <xiongchuhanplace@hotmail.com>
2014-07-17 21:33:29 +0800

    李清李清我爱你，就像陈睿爱大米

---
+++ iShangkeProject/iShangkeProject-Service/src/main/java/com/ishangke/edunav/manager/impl/AddressManagerImpl.java
@@ -0,0 +1,44 @@
+package com.ishangke.edunav.manager.impl;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.ishangke.edunav.commoncontract.model.AddressBo;
+import com.ishangke.edunav.commoncontract.model.PaginationBo;
+import com.ishangke.edunav.commoncontract.model.PartnerBo;
+import com.ishangke.edunav.commoncontract.model.UserBo;
+import com.ishangke.edunav.manager.AddressManager;
+
+public class AddressManagerImpl implements AddressManager{
+	private static final Logger LOGGER = LoggerFactory.getLogger(AccountManagerImpl.class);
+	@Override
+	public AddressBo createAddress(AddressBo addressBo, UserBo userBo,
+			PartnerBo partnerBo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AddressBo updateAddress(AddressBo addressBo, UserBo userBo,
+			PartnerBo partnerBo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AddressBo deleteAddress(AddressBo addressBo, UserBo userBo,
+			PartnerBo partnerBo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<AddressBo> query(AddressBo addressBo, UserBo userBo,
+			PaginationBo paginationBo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ iShangkeProject/iShangkeProject-Service/src/main/java/com/ishangke/edunav/service/impl/AddressServiceImpl.java
@@ -0,0 +1,82 @@
+package com.ishangke.edunav.service.impl;
+
+import java.util.List;
+
+import org.apache.thrift.TException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ishangke.edunav.commoncontract.model.AddressBo;
+import com.ishangke.edunav.commoncontract.model.BusinessExceptionBo;
+import com.ishangke.edunav.commoncontract.model.PaginationBo;
+import com.ishangke.edunav.commoncontract.model.PartnerBo;
+import com.ishangke.edunav.commoncontract.model.UserBo;
+import com.ishangke.edunav.commoncontract.service.AddressService;
+import com.ishangke.edunav.manager.AddressManager;
+import com.ishangke.edunav.manager.common.ManagerErrorCode;
+import com.ishangke.edunav.manager.exception.ManagerException;
+
+public class AddressServiceImpl implements AddressService.Iface{
+	private static final Logger LOGGER = LoggerFactory.getLogger(ActivityServiceImpl.class);
+	
+	@Autowired
+	private AddressManager addressManager;
+	
+	@Override
+	public AddressBo createAddress(AddressBo addressBo, UserBo userBo,
+			PartnerBo partnerBo) throws BusinessExceptionBo, TException {
+		try{
+			return addressManager.createAddress(addressBo, userBo, partnerBo);
+		}catch(ManagerException e){
+			LOGGER.info(e.getMessage(), e);
+			BusinessExceptionBo exception = new BusinessExceptionBo();
+            exception.setErrorCode(ManagerErrorCode.ADDRESS_CREATE_ERROR);
+            exception.setMessageKey(ManagerErrorCode.ADDRESS_CREATE_ERROR_KEY);
+            throw exception;
+		}
+	}
+
+	@Override
+	public AddressBo updateAddress(AddressBo addressBo, UserBo userBo,
+			PartnerBo partnerBo) throws BusinessExceptionBo, TException {
+		try{
+			return addressManager.updateAddress(addressBo, userBo, partnerBo);
+		}catch(ManagerException e){
+			LOGGER.info(e.getMessage(), e);
+			BusinessExceptionBo exception = new BusinessExceptionBo();
+            exception.setErrorCode(ManagerErrorCode.ADDRESS_UPDATE_ERROR);
+            exception.setMessageKey(ManagerErrorCode.ADDRESS_UPDATE_ERROR_KEY);
+            throw exception;
+		}
+	}
+
+	@Override
+	public AddressBo deleteAddress(AddressBo addressBo, UserBo userBo,
+			PartnerBo partnerBo) throws BusinessExceptionBo, TException {
+		try{
+			return addressManager.deleteAddress(addressBo, userBo, partnerBo);
+		}catch(ManagerException e){
+			LOGGER.info(e.getMessage(), e);
+			BusinessExceptionBo exception = new BusinessExceptionBo();
+            exception.setErrorCode(ManagerErrorCode.ADDRESS_DELETE_ERROR);
+            exception.setMessageKey(ManagerErrorCode.ADDRESS_DELETE_ERROR_KEY);
+            throw exception;
+		}
+	}
+
+	@Override
+	public List<AddressBo> query(AddressBo addressBo, UserBo userBo,
+			PaginationBo paginationBo) throws BusinessExceptionBo, TException {
+		try{
+			return addressManager.query(addressBo, userBo, paginationBo);
+		}catch(ManagerException e){
+			LOGGER.info(e.getMessage(), e);
+			BusinessExceptionBo exception = new BusinessExceptionBo();
+            exception.setErrorCode(ManagerErrorCode.ADDRESS_QUERY_ERROR);
+            exception.setMessageKey(ManagerErrorCode.ADDRESS_QUERY_ERROR_KEY);
+            throw exception;
+		}
+	}
+
+}
