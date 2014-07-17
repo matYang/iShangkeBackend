7ebbe766239d7e60c4b3f6cdeeff0611c626b75a
matYang <uwse@me.com>
2014-07-17 22:27:25 +0800

    filling up managers

--- iShangkeProject/iShangkeProject-Service/src/main/java/com/ishangke/edunav/manager/CourseTemplateManager.java
+++ iShangkeProject/iShangkeProject-Service/src/main/java/com/ishangke/edunav/manager/CourseTemplateManager.java
@@ -2,6 +2,7 @@ package com.ishangke.edunav.manager;
 
 import java.util.List;
 
+import com.ishangke.edunav.commoncontract.model.BusinessExceptionBo;
 import com.ishangke.edunav.commoncontract.model.CommentCourseTemplateApproveBo;
 import com.ishangke.edunav.commoncontract.model.CommentCourseTemplateRejectBo;
 import com.ishangke.edunav.commoncontract.model.CourseTemplateBo;
@@ -10,123 +11,213 @@ import com.ishangke.edunav.commoncontract.model.PartnerBo;
 import com.ishangke.edunav.commoncontract.model.UserBo;
 
 public interface CourseTemplateManager {
-     /**
-     *  本方法为创建新课程模版<br>
-     *  合作商管理员可以通过此方法创建新课程模版
-     *
-     *  @param  courseTemplateBo    需要被创建的课程模版信息
-     *  @param  partnerBo           合作商信息
-     *  @param  UserBo              调用方法的用户的信息
-     *
-     *  @return 课程模版实体 CourseTemplateBo 
-     *
+    /**
+     * 本方法为创建新课程模版<br>
+     * 合作商管理员可以通过此方法创建新课程模版
+     * 
+     * @param courseTemplateBo
+     *            需要被创建的课程模版信息
+     * @param partnerBo
+     *            合作商信息
+     * @param UserBo
+     *            调用方法的用户的信息
+     * 
+     * @return 课程模版实体 CourseTemplateBo
+     * 
+     * 
+     * @param courseTemplateBo
+     * @param partnerBo
+     * @param userBo
      */
-    CourseTemplateBo createCourseTemplateBo(CourseTemplateBo courseTemplateBo, PartnerBo partnerBo, UserBo userBo);
+    public CourseTemplateBo createCourseTemplateBo(CourseTemplateBo courseTemplateBo, PartnerBo partnerBo, UserBo userBo) throws BusinessExceptionBo, org.apache.thrift.TException;
 
     /**
-     *  本方法为提交课程模版<br>
-     *  合作商管理员可以通过此方法提交课程模版
-     *
-     *  @param  courseTemplateBo    需要被创建的课程模版信息
-     *  @param  partnerBo           合作商信息
-     *  @param  UserBo              调用方法的用户的信息
-     *
-     *  @return  
-     *
+     * 本方法为提交课程模版<br>
+     * 合作商管理员可以通过此方法提交课程模版
+     * 
+     * @param courseTemplateBo
+     *            需要被创建的课程模版信息
+     * @param partnerBo
+     *            合作商信息
+     * @param UserBo
+     *            调用方法的用户的信息
+     * 
+     * @return
+     * 
+     * 
+     * @param courseTemplateBo
+     * @param partnerBo
+     * @param userBo
      */
-    CourseTemplateBo submitCourseTemplate(CourseTemplateBo courseTemplateBo, PartnerBo partnerBo, UserBo userBo); 
+    public CourseTemplateBo submitCourseTemplate(CourseTemplateBo courseTemplateBo, PartnerBo partnerBo, UserBo userBo) throws BusinessExceptionBo, org.apache.thrift.TException;
 
     /**
-     *  本方法为审核通过课程模版<br>
-     *  ishangke管理员可以通过此方法审核通过课程模版
-     *
-     *  @param  courseTemplateBo          需要被审核的课程模版信息
-     *  @param  partnerBo                 合作商信息
-     *  @param  commentTemplateApproveBo  审核通过课程模版评论信息
-     *  @param  userBo                    调用方法的用户的信息
-     *
-     *  @return  
-     *
+     * 本方法为审核通过课程模版<br>
+     * ishangke管理员可以通过此方法审核通过课程模版
+     * 
+     * @param courseTemplateBo
+     *            需要被审核的课程模版信息
+     * @param partnerBo
+     *            合作商信息
+     * @param commentTemplateApproveBo
+     *            审核通过课程模版评论信息
+     * @param userBo
+     *            调用方法的用户的信息
+     * 
+     * @return
+     * 
+     * 
+     * @param courseTemplateBo
+     * @param partnerBo
+     * @param commentCourseTemplateApproveBo
+     * @param userBo
      */
-    CourseTemplateBo approveCourseTemplate(CourseTemplateBo courseTemplateBo, PartnerBo partnerBo, CommentCourseTemplateApproveBo commentCourseTemplateApproveBo, UserBo userBo); 
+    public CourseTemplateBo approveCourseTemplate(CourseTemplateBo courseTemplateBo, PartnerBo partnerBo, CommentCourseTemplateApproveBo commentCourseTemplateApproveBo, UserBo userBo)
+            throws BusinessExceptionBo, org.apache.thrift.TException;
 
     /**
-     *  本方法为审核不通过课程模版<br>
-     *  ishangke管理员可以通过此方法审核不通过课程模版
-     *
-     *  @param  courseTemplateBo          需要被审核的课程模版信息
-     *  @param  partnerBo                 合作商信息
-     *  @param  commentTemplateApproveBo  审核不通过课程模版评论信息
-     *  @param  userBo                    调用方法的用户的信息
-     *
-     *  @return  
-     *
+     * 本方法为审核不通过课程模版<br>
+     * ishangke管理员可以通过此方法审核不通过课程模版
+     * 
+     * @param courseTemplateBo
+     *            需要被审核的课程模版信息
+     * @param partnerBo
+     *            合作商信息
+     * @param commentTemplateApproveBo
+     *            审核不通过课程模版评论信息
+     * @param userBo
+     *            调用方法的用户的信息
+     * 
+     * @return
+     * 
+     * 
+     * @param courseTemplateBo
+     * @param commentCourseTemplateRejectBo
+     * @param partnerBo
+     * @param userBo
      */
-    CourseTemplateBo rejectCourseTemplate(CourseTemplateBo courseTemplateBo, CommentCourseTemplateRejectBo commentCourseTemplateRejectBo, PartnerBo partnerBo, UserBo userBo);
+    public CourseTemplateBo rejectCourseTemplate(CourseTemplateBo courseTemplateBo, CommentCourseTemplateRejectBo commentCourseTemplateRejectBo, PartnerBo partnerBo, UserBo userBo)
+            throws BusinessExceptionBo, org.apache.thrift.TException;
 
     /**
-     *  本方法为下架课程模版<br>
-     *  合作商管理员可以通过此方法下架课程模版
-     *
-     *  @param  courseTemplateBo    需要被下架的课程模版信息
-     *  @param  partnerBo           合作商信息
-     *  @param  userBo              调用方法的用户的信息
-     *
-     *  @return  
-     *
+     * 本方法为下架课程模版<br>
+     * 合作商管理员可以通过此方法下架课程模版
+     * 
+     * @param courseTemplateBo
+     *            需要被下架的课程模版信息
+     * @param partnerBo
+     *            合作商信息
+     * @param userBo
+     *            调用方法的用户的信息
+     * 
+     * @return
+     * 
+     * 
+     * @param courseTemplateBo
+     * @param partnerBo
+     * @param userBo
      */
-    CourseTemplateBo cancelCourseTemplate(CourseTemplateBo courseTemplateBo, PartnerBo partnerBo, UserBo userBo); 
-
-     /**
-     *  本方法为重新上架课程模版<br>
-     *  合作商管理员可以通过此方法重新上架课程模版
-     *
-     *  @param  courseTemplateBo    需要被重新上架的课程模版信息
-     *  @param  partnerBo           合作商信息
-     *  @param  userBo              调用方法的用户的信息
-     *
-     *  @return  
-     *
-     */
-    CourseTemplateBo resubmitCourserTemplate(CourseTemplateBo courseTemplateBo, PartnerBo partnerBo, UserBo userBo); 
+    public CourseTemplateBo cancelCourseTemplate(CourseTemplateBo courseTemplateBo, PartnerBo partnerBo, UserBo userBo) throws BusinessExceptionBo, org.apache.thrift.TException;
 
     /**
-     *  本方法为删除课程模版<br>
-     *  合作商管理员可以通过此方法删除已经下架的课程模版
-     *
-     *  @param  courseTemplateBo    需要被删除的课程模版信息
-     *  @param  partnerBo           合作商信息
-     *  @param  userBo              调用方法的用户的信息
-     *
-     *  @return  
-     *
+     * 本方法为重新上架课程模版<br>
+     * 合作商管理员可以通过此方法重新上架课程模版
+     * 
+     * @param courseTemplateBo
+     *            需要被重新上架的课程模版信息
+     * @param partnerBo
+     *            合作商信息
+     * @param userBo
+     *            调用方法的用户的信息
+     * 
+     * @return
+     * 
+     * 
+     * @param courseTemplateBo
+     * @param partnerBo
+     * @param userBo
      */
-    CourseTemplateBo deleteCourseTempalte(CourseTemplateBo courseTemplateBo, PartnerBo partnerBo, UserBo userBo); 
-
-     /**
-     *  本方法为修改课程模版<br>
-     *  合作商管理员可以通过此方法修改课程模版
-     *
-     *  @param  courseTemplateBo    需要被修改的课程模版信息
-     *  @param  partnerBo           合作商信息
-     *  @param  userBo              调用方法的用户的信息
-     *
-     *  @return  
-     *
-     */
-    CourseTemplateBo updateCourseTemplate(CourseTemplateBo courseTemplateBo, PartnerBo partnerBo, UserBo userBo);
+    public CourseTemplateBo resubmitCourserTemplate(CourseTemplateBo courseTemplateBo, PartnerBo partnerBo, UserBo userBo) throws BusinessExceptionBo, org.apache.thrift.TException;
 
     /**
-     *  本方法为搜索课程模版<br>
-     *  合作商管理员可以通过此方法查询课程模版
-     *
-     *  @param  courseTemplateBo    需要检索和过滤的课程模版信息
-     *  @param  partnerBo           合作商信息
-     *  @param  UserBo              调用方法的用户的信息
-     *  @param  paginationBo        分页信息
-     *
-     *  @return  
-     *
+     * 本方法为删除课程模版<br>
+     * 合作商管理员可以通过此方法删除已经下架的课程模版
+     * 
+     * @param courseTemplateBo
+     *            需要被删除的课程模版信息
+     * @param partnerBo
+     *            合作商信息
+     * @param userBo
+     *            调用方法的用户的信息
+     * 
+     * @return
+     * 
+     * 
+     * @param courseTemplateBo
+     * @param partnerBo
+     * @param userBo
      */
-    List<CourseTemplateBo> queryCourseTempalte(CourseTemplateBo courseTemplateBo, UserBo userBo, PartnerBo partnerBo, PaginationBo paginationBo);
+    public CourseTemplateBo deleteCourseTempalte(CourseTemplateBo courseTemplateBo, PartnerBo partnerBo, UserBo userBo) throws BusinessExceptionBo, org.apache.thrift.TException;
+
+    /**
+     * 本方法为修改课程模版<br>
+     * 合作商管理员可以通过此方法修改课程模版
+     * 
+     * @param courseTemplateBo
+     *            需要被修改的课程模版信息
+     * @param partnerBo
+     *            合作商信息
+     * @param userBo
+     *            调用方法的用户的信息
+     * 
+     * @return
+     * 
+     * 
+     * @param courseTemplateBo
+     * @param partnerBo
+     * @param userBo
+     */
+    public CourseTemplateBo updateCourseTemplate(CourseTemplateBo courseTemplateBo, PartnerBo partnerBo, UserBo userBo) throws BusinessExceptionBo, org.apache.thrift.TException;
+
+    /**
+     * 本方法为搜索课程模版<br>
+     * 合作商管理员可以通过此方法查询课程模版
+     * 
+     * @param courseTemplateBo
+     *            需要检索和过滤的课程模版信息
+     * @param partnerBo
+     *            合作商信息
+     * @param UserBo
+     *            调用方法的用户的信息
+     * @param paginationBo
+     *            分页信息
+     * 
+     * @return
+     * 
+     * 
+     * @param courseTemplateBo
+     * @param userBo
+     * @param partnerBo
+     * @param paginationBo
+     */
+    public List<CourseTemplateBo> queryCourseTempalte(CourseTemplateBo courseTemplateBo, UserBo userBo, PartnerBo partnerBo, PaginationBo paginationBo) throws BusinessExceptionBo,
+            org.apache.thrift.TException;
+
+    /**
+     * 本方法返回课程模版详情<br>
+     * 合作商/管理员可以通过此方法调取
+     * 
+     * @param courseTemplateBo
+     *            课程模板id
+     * @param userBo
+     *            调用方法的用户的信息
+     * 
+     * @return
+     * 
+     * 
+     * @param courseTemplateBo
+     * @param userBo
+     */
+    public List<CourseTemplateBo> queryCourseTempalteById(CourseTemplateBo courseTemplateBo, UserBo userBo) throws BusinessExceptionBo, org.apache.thrift.TException;
+
 }
--- iShangkeProject/iShangkeProject-Service/src/main/java/com/ishangke/edunav/manager/PartnerManager.java
+++ iShangkeProject/iShangkeProject-Service/src/main/java/com/ishangke/edunav/manager/PartnerManager.java
@@ -1,5 +1,83 @@
 package com.ishangke.edunav.manager;
 
+import java.util.List;
+
+import com.ishangke.edunav.commoncontract.model.BusinessExceptionBo;
+import com.ishangke.edunav.commoncontract.model.PaginationBo;
+import com.ishangke.edunav.commoncontract.model.PartnerBo;
+import com.ishangke.edunav.commoncontract.model.UserBo;
+
 public interface PartnerManager {
+
+    /**
+     * 查询合作商信息。<br>
+     * 用户可以看到合作商相关信息
+     * 
+     * @param partnerBo
+     *            需要检索的合作商信息
+     * @param paginationBo
+     *            分页信息
+     * @param userBo
+     *            用户信息
+     * 
+     * @return 合作商实体 PartnerBo 列表
+     * 
+     * 
+     * @param partnerBo
+     * @param paginationBo
+     * @param userBo
+     */
+    public List<PartnerBo> query(PartnerBo partnerBo, PaginationBo paginationBo, UserBo userBo) throws BusinessExceptionBo, org.apache.thrift.TException;
+
+    /**
+     * 查询合作商完整信息。<br>
+     * 用户可以看到合作商完整信息
+     * 
+     * @param partnerBo
+     *            合作商Id
+     * @param UserBo
+     *            用户信息
+     * 
+     * @return 合作商实体 PartnerBo
+     * 
+     * 
+     * @param partnerBo
+     * @param userBo
+     */
+    public PartnerBo queryById(PartnerBo partnerBo, UserBo userBo) throws BusinessExceptionBo, org.apache.thrift.TException;
+
+    /**
+     * 更新合作商信息。<br>
+     * 管理员可以更新合作商的信息
+     * 
+     * @param partnerBo
+     *            需要更新的合作商信息
+     * @param userBo
+     *            调用方法的管理员信息
+     * 
+     * @return 更新过后的合作商信息
+     * 
+     * 
+     * @param partnerBo
+     * @param userBo
+     */
+    public PartnerBo updatePartner(PartnerBo partnerBo, UserBo userBo) throws BusinessExceptionBo, org.apache.thrift.TException;
+
+    /**
+     * 创建合作商信息。<br>
+     * 管理员可以录入新合作商的信息
+     * 
+     * @param partnerBo
+     *            需要创建的新合作商信息
+     * @param userBo
+     *            调用方法的管理员信息
+     * 
+     * @return 创建完毕的合作商信息
+     * 
+     * 
+     * @param partnerBo
+     * @param userBo
+     */
+    public PartnerBo createPartner(PartnerBo partnerBo, UserBo userBo) throws BusinessExceptionBo, org.apache.thrift.TException;
 
 }
