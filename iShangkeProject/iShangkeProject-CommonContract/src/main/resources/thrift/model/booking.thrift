de1a17c5c8207189fc9ac8dbc57591f6b0fb53d1
Aaron <467517168@qq.com>
2014-07-18 10:07:25 +0800

    BookingBo不全

--- iShangkeProject/iShangkeProject-CommonContract/src/main/resources/thrift/model/booking.thrift
+++ iShangkeProject/iShangkeProject-CommonContract/src/main/resources/thrift/model/booking.thrift
@@ -4,24 +4,42 @@ namespace java com.ishangke.edunav.commoncontract.model
  * BookingBo
  */
 struct BookingBo {
-  1: i32 id
-  2: i32 type;
-  3: i32 attachedId;
-  4: i32 submitUserId;
-  5: i32 approveUserId;
-  6: i32 preStatus;
-  7: i32 postStatus;
-  8: string comment;
-  9: i64 createTime;
-  10: set<i32> idSet;
-  11: set<i32> typeSet;
-  12: set<i32> attachedIdSet;
-  13: set<i32> submitUserIdSet;
-  14: set<i32> approveUserIdSet;
-  15: set<i32> preStatusSet;
-  16: set<i32> postStatusSet;
-  17: i64 createTimeStart;
-  18: i64 createTimeEnd;
+    1: i32 id
+    2: i32 userId
+    3: i32 courseId
+    4: i32 courseTemplateId
+    5: string reference
+    6: string name
+    7: string phone
+    8: string email
+    9: i32 status
+    10: i64 scheduledTime
+    11: i64 noRefundDate
+    12: i64 cashbackDate
+    13: i32 type
+    14: string note
+    15: double cashbackAmount
+    16: i64 lastModifyTime
+    17: i64 createTime
+    18: i32 enabled
+    19: set<i32> idSet
+    20: set<i32> courseIdSet
+    21: set<i32> courseTemplateIdSet
+    22: set<i32> userIdSet
+    23: set<i32> statusSet
+    24: i64 scheduledTimeStart
+    25: i64 scheduledTimeEnd
+    26: i64 noRefundDateStart
+    27: i64 noRefundDateEnd
+    28: i64 cashbackDateStart
+    29: i64 cashbackDateEnd
+    30: set<i32> typeSet
+    31: double cashbackAmountStart
+    32: double cashbackAmountEnd
+    33: i64 lastModifyTimeStart
+    34: i64 lastModifyTimeEnd
+    35: i64 createTimeStart
+    36: i64 createTimeEnd
 }
 
 /**
