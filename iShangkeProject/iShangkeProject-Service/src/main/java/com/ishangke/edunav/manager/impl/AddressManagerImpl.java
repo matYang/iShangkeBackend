1abb2cb3f634add5328ab2cfbb2c4654ad42a636
Harry Xiong <xiongchuhanplace@hotmail.com>
2014-07-18 21:24:00 +0800

    初步的..ManagerImpl.. 还需更改

--- iShangkeProject/iShangkeProject-Service/src/main/java/com/ishangke/edunav/manager/impl/AddressManagerImpl.java
+++ iShangkeProject/iShangkeProject-Service/src/main/java/com/ishangke/edunav/manager/impl/AddressManagerImpl.java
@@ -4,38 +4,158 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.ishangke.edunav.commoncontract.model.AddressBo;
 import com.ishangke.edunav.commoncontract.model.PaginationBo;
 import com.ishangke.edunav.commoncontract.model.PartnerBo;
 import com.ishangke.edunav.commoncontract.model.UserBo;
+import com.ishangke.edunav.dataaccess.common.PaginationEntity;
+import com.ishangke.edunav.dataaccess.mapper.AddressEntityExtMapper;
+import com.ishangke.edunav.dataaccess.mapper.PartnerEntityExtMapper;
+import com.ishangke.edunav.dataaccess.mapper.UserEntityExtMapper;
+import com.ishangke.edunav.dataaccess.model.AddressEntityExt;
+import com.ishangke.edunav.dataaccess.model.PartnerEntityExt;
+import com.ishangke.edunav.dataaccess.model.UserEntityExt;
 import com.ishangke.edunav.manager.AddressManager;
+import com.ishangke.edunav.manager.converter.AddressConverter;
+import com.ishangke.edunav.manager.converter.PaginationConverter;
+import com.ishangke.edunav.manager.converter.PartnerConverter;
+import com.ishangke.edunav.manager.converter.UserConverter;
+import com.ishangke.edunav.manager.exception.ManagerException;
 
 public class AddressManagerImpl implements AddressManager {
     private static final Logger LOGGER = LoggerFactory.getLogger(AddressManagerImpl.class);
 
+    @Autowired
+    private UserEntityExtMapper userEntityExtMapper;
+    @Autowired
+    private PartnerEntityExtMapper partnerEntityExtMapper;
+    @Autowired
+    private AddressEntityExtMapper addressEntityExtMapper;
+
     @Override
     public AddressBo createAddress(AddressBo addressBo, UserBo userBo, PartnerBo partnerBo) {
-        // TODO Auto-generated method stub
-        return null;
+        // Check Null
+        if (partnerBo == null) {
+            throw new ManagerException("PartnerBo is null");
+        }
+        if (userBo == null) {
+            throw new ManagerException("UserBo is null");
+        }
+        if (addressBo == null) {
+            throw new ManagerException("Address is null");
+        }
+
+        // Convert
+        UserEntityExt userEntity = UserConverter.fromBo(userBo);
+        PartnerEntityExt partnerEntity = PartnerConverter.fromBo(partnerBo);
+        AddressEntityExt addressEntity = AddressConverter.fromBo(addressBo);
+
+        try {
+            addressEntity.setPartnerId(partnerEntity.getId());
+            // TODO权限
+            int result = 0;
+            result = addressEntityExtMapper.add(addressEntity);
+            if (result > 0) {
+                return AddressConverter.toBo(addressEntity);
+            } else
+                throw new ManagerException("Address Create Failed");
+        } catch (Throwable t) {
+            throw new ManagerException("Address Create Failed");
+        }
+
     }
 
     @Override
     public AddressBo updateAddress(AddressBo addressBo, UserBo userBo, PartnerBo partnerBo) {
-        // TODO Auto-generated method stub
-        return null;
+        // Check Null
+        if (partnerBo == null) {
+            throw new ManagerException("PartnerBo is null");
+        }
+        if (userBo == null) {
+            throw new ManagerException("UserBo is null");
+        }
+        if (addressBo == null) {
+            throw new ManagerException("Address is null");
+        }
+
+        // Convert
+        UserEntityExt userEntity = UserConverter.fromBo(userBo);
+        PartnerEntityExt partnerEntity = PartnerConverter.fromBo(partnerBo);
+        AddressEntityExt addressEntity = AddressConverter.fromBo(addressBo);
+        AddressBo result = null;
+
+        try {
+            if (partnerEntity.getId() != addressEntity.getPartnerId()) {
+                throw new ManagerException("此合作商不是该地址的合作商");
+            }
+            // TODO权限
+            addressEntityExtMapper.update(addressEntity);
+            result = AddressConverter.toBo(addressEntity);
+            return result;
+        } catch (Throwable t) {
+            throw new ManagerException("Address Update Failed");
+        }
     }
 
     @Override
     public AddressBo deleteAddress(AddressBo addressBo, UserBo userBo, PartnerBo partnerBo) {
-        // TODO Auto-generated method stub
-        return null;
+        // Check Null
+        if (partnerBo == null) {
+            throw new ManagerException("PartnerBo is null");
+        }
+        if (userBo == null) {
+            throw new ManagerException("UserBo is null");
+        }
+        if (addressBo == null) {
+            throw new ManagerException("Address is null");
+        }
+
+        // Convert
+        UserEntityExt userEntity = UserConverter.fromBo(userBo);
+        PartnerEntityExt partnerEntity = PartnerConverter.fromBo(partnerBo);
+        AddressEntityExt addressEntity = AddressConverter.fromBo(addressBo);
+        AddressBo result = null;
+
+        try {
+            if (partnerEntity.getId() != addressEntity.getPartnerId()) {
+                throw new ManagerException("此合作商不是该地址的合作商");
+            }
+            // TODO权限
+            addressEntityExtMapper.deleteById(addressEntity.getId());
+            result = AddressConverter.toBo(addressEntity);
+            return result;
+        } catch (Throwable t) {
+            throw new ManagerException("Address Delete Failed");
+        }
     }
 
     @Override
     public List<AddressBo> query(AddressBo addressBo, UserBo userBo, PaginationBo paginationBo) {
-        // TODO Auto-generated method stub
-        return null;
+        if (userBo == null) {
+            throw new ManagerException("UserBo is null");
+        }
+        if (addressBo == null) {
+            throw new ManagerException("Address is null");
+        }
+
+        // Convert
+        UserEntityExt userEntity = UserConverter.fromBo(userBo);
+        PaginationEntity pageEntity = PaginationConverter.fromBo(paginationBo);
+        AddressEntityExt addressEntity = AddressConverter.fromBo(addressBo);
+        List<AddressEntityExt> addressList = null;
+        List<AddressBo> resultList = null;
+        try {
+            // TODO权限
+            addressList = addressEntityExtMapper.list(addressEntity, pageEntity);
+            for (AddressEntityExt addressPo : addressList) {
+                resultList.add(AddressConverter.toBo(addressPo));
+            }
+            return resultList;
+        } catch (Throwable t) {
+            throw new ManagerException("Address Query Failed");
+        }
     }
 
 }
--- iShangkeProject/iShangkeProject-Service/src/main/java/com/ishangke/edunav/manager/impl/ContactManagerImpl.java
+++ iShangkeProject/iShangkeProject-Service/src/main/java/com/ishangke/edunav/manager/impl/ContactManagerImpl.java
@@ -4,37 +4,140 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.ishangke.edunav.commoncontract.model.ContactBo;
 import com.ishangke.edunav.commoncontract.model.PaginationBo;
 import com.ishangke.edunav.commoncontract.model.UserBo;
+import com.ishangke.edunav.dataaccess.common.PaginationEntity;
+import com.ishangke.edunav.dataaccess.mapper.ContactEntityExtMapper;
+import com.ishangke.edunav.dataaccess.model.ContactEntityExt;
+import com.ishangke.edunav.dataaccess.model.UserEntityExt;
 import com.ishangke.edunav.manager.ContactManager;
+import com.ishangke.edunav.manager.converter.ContactConverter;
+import com.ishangke.edunav.manager.converter.PaginationConverter;
+import com.ishangke.edunav.manager.converter.UserConverter;
+import com.ishangke.edunav.manager.exception.ManagerException;
 
 public class ContactManagerImpl implements ContactManager {
     private static final Logger LOGGER = LoggerFactory.getLogger(ContactManagerImpl.class);
 
+    @Autowired
+    private ContactEntityExtMapper contactEntityExtMapper;
+
     @Override
     public ContactBo createContact(ContactBo contactBo, UserBo userBo) {
-        // TODO Auto-generated method stub
-        return null;
+        // Check Null
+        if (contactBo == null) {
+            throw new ManagerException("contactBo is null");
+        }
+        if (userBo == null) {
+            throw new ManagerException("userBo is null");
+        }
+
+        // Convert
+        ContactEntityExt convertEntity = ContactConverter.fromBo(contactBo);
+        UserEntityExt userEntity = UserConverter.fromBo(userBo);
+
+        if (convertEntity.getUserId() != userEntity.getId()) {
+            throw new ManagerException("此用户无法创建该常用联系人");
+        }
+
+        try {
+            int result = 0;
+            result = contactEntityExtMapper.add(convertEntity);
+            if (result > 0) {
+                return ContactConverter.toBo(convertEntity);
+            } else {
+                throw new ManagerException("Contact Create Failed");
+            }
+        } catch (Throwable t) {
+            throw new ManagerException("Contact Create Failed");
+        }
     }
 
     @Override
     public ContactBo updateContact(ContactBo contactBo, UserBo userBo) {
-        // TODO Auto-generated method stub
-        return null;
+        // Check Null
+        if (contactBo == null) {
+            throw new ManagerException("contactBo is null");
+        }
+        if (userBo == null) {
+            throw new ManagerException("userBo is null");
+        }
+
+        // Convert
+        ContactEntityExt convertEntity = ContactConverter.fromBo(contactBo);
+        UserEntityExt userEntity = UserConverter.fromBo(userBo);
+
+        if (convertEntity.getUserId() != userEntity.getId()) {
+            throw new ManagerException("此用户无法更新该常用联系人");
+        }
+
+        try {
+            contactEntityExtMapper.update(convertEntity);
+            return ContactConverter.toBo(convertEntity);
+        } catch (Throwable t) {
+            throw new ManagerException("Contact Update Failed");
+        }
     }
 
     @Override
     public ContactBo deleteContact(ContactBo contactBo, UserBo userBo) {
-        // TODO Auto-generated method stub
-        return null;
+        // Check Null
+        if (contactBo == null) {
+            throw new ManagerException("contactBo is null");
+        }
+        if (userBo == null) {
+            throw new ManagerException("userBo is null");
+        }
+
+        // Convert
+        ContactEntityExt convertEntity = ContactConverter.fromBo(contactBo);
+        UserEntityExt userEntity = UserConverter.fromBo(userBo);
+
+        if (convertEntity.getUserId() != userEntity.getId()) {
+            throw new ManagerException("此用户无法删除该常用联系人");
+        }
+
+        try {
+            contactEntityExtMapper.deleteById(convertEntity.getId());
+            return ContactConverter.toBo(convertEntity);
+        } catch (Throwable t) {
+            throw new ManagerException("Contact Delete Failed");
+        }
     }
 
     @Override
     public List<ContactBo> query(ContactBo contactBo, UserBo userBo, PaginationBo paginationBo) {
-        // TODO Auto-generated method stub
-        return null;
+        // Check Null
+        if (contactBo == null) {
+            throw new ManagerException("contactBo is null");
+        }
+        if (userBo == null) {
+            throw new ManagerException("userBo is null");
+        }
+
+        // Convert
+        ContactEntityExt convertEntity = ContactConverter.fromBo(contactBo);
+        UserEntityExt userEntity = UserConverter.fromBo(userBo);
+        PaginationEntity pageEntity = PaginationConverter.fromBo(paginationBo);
+        List<ContactEntityExt> contactList = null;
+        List<ContactBo> resultList = null;
+
+        if (convertEntity.getUserId() != userEntity.getId()) {
+            throw new ManagerException("此用户无法查找该常用联系人");
+        }
+
+        try {
+            contactList = contactEntityExtMapper.list(convertEntity, pageEntity);
+            for (ContactEntityExt contactPo : contactList) {
+                resultList.add(ContactConverter.toBo(contactPo));
+            }
+            return resultList;
+        } catch (Throwable t) {
+            throw new ManagerException("Contact Query Failed");
+        }
     }
 
 }
