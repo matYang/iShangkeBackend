fba95da1876f580a9cd34574bdd4a5809373986d
matYang <uwse@me.com>
2014-07-20 00:41:39 +0800

    adding template for tree conversion

---
+++ iShangkeProject/iShangkeProject-Common/src/main/java/com/ishangke/edunav/common/tree/TreeParser.java
@@ -0,0 +1,12 @@
+package com.ishangke.edunav.common.tree;
+
+public class TreeParser {
+
+    // public static JSONObject parseJSON() {
+    // return null;
+    // }
+
+    public static <T> SDTree<T> parseTree(T t) {
+        return null;
+    }
+}
