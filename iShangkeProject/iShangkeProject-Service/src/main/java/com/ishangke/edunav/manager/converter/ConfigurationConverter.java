ba19bdaaaf1da549d1eb9f2ed14bc5342b1d63df
Aaron <467517168@qq.com>
2014-07-21 00:06:29 +0800

    add configuration converter

--- iShangkeProject/iShangkeProject-Service/src/main/java/com/ishangke/edunav/manager/converter/ConfigurationConverter.java
+++ iShangkeProject/iShangkeProject-Service/src/main/java/com/ishangke/edunav/manager/converter/ConfigurationConverter.java
@@ -6,11 +6,17 @@ import com.ishangke.edunav.dataaccess.model.ConfigurationEntityExt;
 public class ConfigurationConverter {
     public static ConfigurationBo toBo(ConfigurationEntityExt e) {
         ConfigurationBo ConfigurationBo = new ConfigurationBo();
+        ConfigurationBo.setConfigData(e.getConfigData());
+        ConfigurationBo.setId(e.getId());
+        ConfigurationBo.setName(e.getName());
         return ConfigurationBo;
     }
 
     public static ConfigurationEntityExt fromBo(ConfigurationBo bo) {
         ConfigurationEntityExt ConfigurationEntityExt = new ConfigurationEntityExt();
+        ConfigurationEntityExt.setConfigData(bo.getConfigData());
+        ConfigurationEntityExt.setId(bo.getId());
+        ConfigurationEntityExt.setName(bo.getName());
         return ConfigurationEntityExt;
     }
 }
