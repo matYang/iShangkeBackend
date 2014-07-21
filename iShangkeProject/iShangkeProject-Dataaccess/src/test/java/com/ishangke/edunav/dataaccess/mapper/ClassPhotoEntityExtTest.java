8934122b641918289a09b28ed4d5afe2922fac22
liqing0216 <1670990633@qq.com>
2014-07-21 10:08:10 +0800

    清哥昨晚加班了

--- iShangkeProject/iShangkeProject-Dataaccess/src/test/java/com/ishangke/edunav/dataaccess/mapper/ClassPhotoEntityExtTest.java
+++ iShangkeProject/iShangkeProject-Dataaccess/src/test/java/com/ishangke/edunav/dataaccess/mapper/ClassPhotoEntityExtTest.java
@@ -18,11 +18,12 @@ import com.ishangke.edunav.dataaccess.common.DateUtility;
 import com.ishangke.edunav.dataaccess.common.OrderByEntity;
 import com.ishangke.edunav.dataaccess.common.PaginationEntity;
 import com.ishangke.edunav.dataaccess.model.ClassPhotoEntityExt;
+import com.ishangke.edunav.dataaccess.model.CouponEntityExt;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath*:applicationContext-dataaccessUT.xml" })
 @Transactional
-public class ClassPhotoEntityExtTest extends BaseTest{
+public class ClassPhotoEntityExtTest extends BaseTest {
     @Autowired
     private ClassPhotoEntityExtMapper classPhotoEntityExtMapper;
     @Autowired
@@ -33,7 +34,7 @@ public class ClassPhotoEntityExtTest extends BaseTest{
     private CourseClassPhotoEntityExtMapper courseClassPhotoEntityExtMapper;
     @Autowired
     private CourseTemplateClassPhotoEntityExtMapper courseTemplateClassPhotoEntityExtMapper;
-    
+
     @Test
     public void testAdd() {
         ClassPhotoEntityExt classPhotoEntityExt = new ClassPhotoEntityExt();
@@ -61,125 +62,135 @@ public class ClassPhotoEntityExtTest extends BaseTest{
         classPhotoEntityExtMapper.deleteById(classPhotoEntityExt.getId());
         Assert.assertSame(classPhotoEntityExtMapper.getCount(), oldcount - 1);
     }
-    
-//    @Test
-//    public void testList(){        
-//        ClassPhotoEntityExt classPhotoEntityExt = new ClassPhotoEntityExt();
-//        classPhotoEntityExt.setCreateTime(DateUtility.getCurTimeInstance());
-//        classPhotoEntityExt.setLastModifyTime(DateUtility.getCurTimeInstance());
-//        classPhotoEntityExt.setPartnerId(1);        
-//        classPhotoEntityExt.setEnabled(1);
-//        classPhotoEntityExt.setDeleted(0);     
-//        classPhotoEntityExtMapper.add(classPhotoEntityExt);
-//        int classPhotoId = classPhotoEntityExt.getId();
-//        
-//        List<ClassPhotoEntityExt> list = classPhotoEntityExtMapper.list(classPhotoEntityExt, null);
-//        Assert.assertSame(list.size(),1);
-//        
-//        int listSize = classPhotoEntityExtMapper.getListCount(classPhotoEntityExt);
-//        Assert.assertSame(listSize,1);
-//        
-//        PaginationEntity page = new PaginationEntity();
-//        page.setOffset(0);
-//        page.setSize(1);
-//        
-//        list = classPhotoEntityExtMapper.list(classPhotoEntityExt, page);
-//        Assert.assertSame(list.size(),1);
-//        
-//        
-//        /* Get By PartnerId */
-//        list = classPhotoEntityExtMapper.listClassPhotoByPartnerId(1);
-//        Assert.assertSame(list.size(),2);
-//              
-//        @SuppressWarnings("unused")
-//        List<ClassPhotoEntityExt> clist = null;
-//        /* Get By CourseId */
-//        CourseTemplateEntityExt courseTemplateEntityExt = new CourseTemplateEntityExt();      
-//        courseTemplateEntityExt.setCreateTime(DateUtility.getCurTimeInstance());
-//        courseTemplateEntityExt.setLastModifyTime(DateUtility.getCurTimeInstance());
-//        courseTemplateEntityExt.setCategoryId(1);
-//        courseTemplateEntityExt.setLocationId(1);
-//        courseTemplateEntityExt.setCircleId(1);
-//        courseTemplateEntityExt.setPartnerId(1);
-//        courseTemplateEntityExt.setAddressId(1);
-//        courseTemplateEntityExt.setEnabled(1);
-//        courseTemplateEntityExt.setDeleted(0);
-//        courseTemplateEntityExtMapper.add(courseTemplateEntityExt);
-//        int courseTemplateId = courseTemplateEntityExt.getId();
-//        
-//        CourseEntityExt courseEntityExt = new CourseEntityExt();     
-//        courseEntityExt.setCreateTime(DateUtility.getCurTimeInstance());
-//        courseEntityExt.setLastModifyTime(DateUtility.getCurTimeInstance());
-//        courseEntityExt.setCourseTemplateId(courseTemplateId);
-//        courseEntityExt.setCategoryId(1);
-//        courseEntityExt.setLocationId(1);
-//        courseEntityExt.setCircleId(1);
-//        courseEntityExt.setPartnerId(1);
-//        courseEntityExt.setAddressId(1);
-//        courseEntityExt.setEnabled(1);
-//        courseEntityExt.setDeleted(0);
-//        courseEntityExtMapper.add(courseEntityExt);
-//        int courseId = courseEntityExt.getId();
-//        
-//        CourseClassPhotoEntityExt courseClassPhotoEntityExt = new CourseClassPhotoEntityExt();
-//        courseClassPhotoEntityExt.setCourseId(courseId);
-//        courseClassPhotoEntityExt.setClassPhotoId(classPhotoId);
-//        courseClassPhotoEntityExt.setCreateTime(DateUtility.getCurTimeInstance());
-//        courseClassPhotoEntityExt.setDeleted(0);
-//        courseClassPhotoEntityExtMapper.add(courseClassPhotoEntityExt);
-//        
-//        list = classPhotoEntityExtMapper.listClassPhotoByCourseId(courseId);
-//        Assert.assertSame(list.size(),1);
-//        
-//        /* Get By CourseTemplateId */
-//        CourseTemplateClassPhotoEntityExt courseTemplateClassPhotoEntityExt = new CourseTemplateClassPhotoEntityExt();
-//        courseTemplateClassPhotoEntityExt.setCourseTemplateId(courseTemplateId);
-//        courseTemplateClassPhotoEntityExt.setClassPhotoId(classPhotoId);
-//        courseTemplateClassPhotoEntityExt.setCreateTime(DateUtility.getCurTimeInstance());
-//        courseTemplateClassPhotoEntityExt.setDeleted(0);
-//        courseTemplateClassPhotoEntityExtMapper.add(courseTemplateClassPhotoEntityExt);
-//        
-//        list = classPhotoEntityExtMapper.listClassPhotoByCourseTempleteId(courseTemplateId);
-//        Assert.assertSame(list.size(),1);      
-//        
-//    }
-//    
-//    @Test
-//    public void testUpdate(){
-//        ClassPhotoEntityExt classPhotoEntityExt = new ClassPhotoEntityExt();
-//        classPhotoEntityExt.setCreateTime(DateUtility.getCurTimeInstance());
-//        classPhotoEntityExt.setLastModifyTime(DateUtility.getCurTimeInstance());
-//        classPhotoEntityExt.setEnabled(1);
-//        classPhotoEntityExt.setDeleted(0);
-//        classPhotoEntityExt.setPartnerId(1);
-//        classPhotoEntityExtMapper.add(classPhotoEntityExt);
-//        
-//        
-//        List<ClassPhotoEntityExt> list = classPhotoEntityExtMapper.list(classPhotoEntityExt, null);
-//        String name = "李清";
-//        list.get(0).setImgUrl(name);
-//        
-//        classPhotoEntityExtMapper.update(list.get(0));
-//        list = classPhotoEntityExtMapper.list(list.get(0), null);
-//        if(list.get(0).getImgUrl().equals(name)){
-//            //Passed;
-//        }else fail();
-//    }
+
+    // @Test
+    // public void testList(){
+    // ClassPhotoEntityExt classPhotoEntityExt = new ClassPhotoEntityExt();
+    // classPhotoEntityExt.setCreateTime(DateUtility.getCurTimeInstance());
+    // classPhotoEntityExt.setLastModifyTime(DateUtility.getCurTimeInstance());
+    // classPhotoEntityExt.setPartnerId(1);
+    // classPhotoEntityExt.setEnabled(1);
+    // classPhotoEntityExt.setDeleted(0);
+    // classPhotoEntityExtMapper.add(classPhotoEntityExt);
+    // int classPhotoId = classPhotoEntityExt.getId();
+    //
+    // List<ClassPhotoEntityExt> list =
+    // classPhotoEntityExtMapper.list(classPhotoEntityExt, null);
+    // Assert.assertSame(list.size(),1);
+    //
+    // int listSize =
+    // classPhotoEntityExtMapper.getListCount(classPhotoEntityExt);
+    // Assert.assertSame(listSize,1);
+    //
+    // PaginationEntity page = new PaginationEntity();
+    // page.setOffset(0);
+    // page.setSize(1);
+    //
+    // list = classPhotoEntityExtMapper.list(classPhotoEntityExt, page);
+    // Assert.assertSame(list.size(),1);
+    //
+    //
+    // /* Get By PartnerId */
+    // list = classPhotoEntityExtMapper.listClassPhotoByPartnerId(1);
+    // Assert.assertSame(list.size(),2);
+    //
+    // @SuppressWarnings("unused")
+    // List<ClassPhotoEntityExt> clist = null;
+    // /* Get By CourseId */
+    // CourseTemplateEntityExt courseTemplateEntityExt = new
+    // CourseTemplateEntityExt();
+    // courseTemplateEntityExt.setCreateTime(DateUtility.getCurTimeInstance());
+    // courseTemplateEntityExt.setLastModifyTime(DateUtility.getCurTimeInstance());
+    // courseTemplateEntityExt.setCategoryId(1);
+    // courseTemplateEntityExt.setLocationId(1);
+    // courseTemplateEntityExt.setCircleId(1);
+    // courseTemplateEntityExt.setPartnerId(1);
+    // courseTemplateEntityExt.setAddressId(1);
+    // courseTemplateEntityExt.setEnabled(1);
+    // courseTemplateEntityExt.setDeleted(0);
+    // courseTemplateEntityExtMapper.add(courseTemplateEntityExt);
+    // int courseTemplateId = courseTemplateEntityExt.getId();
+    //
+    // CourseEntityExt courseEntityExt = new CourseEntityExt();
+    // courseEntityExt.setCreateTime(DateUtility.getCurTimeInstance());
+    // courseEntityExt.setLastModifyTime(DateUtility.getCurTimeInstance());
+    // courseEntityExt.setCourseTemplateId(courseTemplateId);
+    // courseEntityExt.setCategoryId(1);
+    // courseEntityExt.setLocationId(1);
+    // courseEntityExt.setCircleId(1);
+    // courseEntityExt.setPartnerId(1);
+    // courseEntityExt.setAddressId(1);
+    // courseEntityExt.setEnabled(1);
+    // courseEntityExt.setDeleted(0);
+    // courseEntityExtMapper.add(courseEntityExt);
+    // int courseId = courseEntityExt.getId();
+    //
+    // CourseClassPhotoEntityExt courseClassPhotoEntityExt = new
+    // CourseClassPhotoEntityExt();
+    // courseClassPhotoEntityExt.setCourseId(courseId);
+    // courseClassPhotoEntityExt.setClassPhotoId(classPhotoId);
+    // courseClassPhotoEntityExt.setCreateTime(DateUtility.getCurTimeInstance());
+    // courseClassPhotoEntityExt.setDeleted(0);
+    // courseClassPhotoEntityExtMapper.add(courseClassPhotoEntityExt);
+    //
+    // list = classPhotoEntityExtMapper.listClassPhotoByCourseId(courseId);
+    // Assert.assertSame(list.size(),1);
+    //
+    // /* Get By CourseTemplateId */
+    // CourseTemplateClassPhotoEntityExt courseTemplateClassPhotoEntityExt = new
+    // CourseTemplateClassPhotoEntityExt();
+    // courseTemplateClassPhotoEntityExt.setCourseTemplateId(courseTemplateId);
+    // courseTemplateClassPhotoEntityExt.setClassPhotoId(classPhotoId);
+    // courseTemplateClassPhotoEntityExt.setCreateTime(DateUtility.getCurTimeInstance());
+    // courseTemplateClassPhotoEntityExt.setDeleted(0);
+    // courseTemplateClassPhotoEntityExtMapper.add(courseTemplateClassPhotoEntityExt);
+    //
+    // list =
+    // classPhotoEntityExtMapper.listClassPhotoByCourseTempleteId(courseTemplateId);
+    // Assert.assertSame(list.size(),1);
+    //
+    // }
+    //
+    // @Test
+    // public void testUpdate(){
+    // ClassPhotoEntityExt classPhotoEntityExt = new ClassPhotoEntityExt();
+    // classPhotoEntityExt.setCreateTime(DateUtility.getCurTimeInstance());
+    // classPhotoEntityExt.setLastModifyTime(DateUtility.getCurTimeInstance());
+    // classPhotoEntityExt.setEnabled(1);
+    // classPhotoEntityExt.setDeleted(0);
+    // classPhotoEntityExt.setPartnerId(1);
+    // classPhotoEntityExtMapper.add(classPhotoEntityExt);
+    //
+    //
+    // List<ClassPhotoEntityExt> list =
+    // classPhotoEntityExtMapper.list(classPhotoEntityExt, null);
+    // String name = "李清";
+    // list.get(0).setImgUrl(name);
+    //
+    // classPhotoEntityExtMapper.update(list.get(0));
+    // list = classPhotoEntityExtMapper.list(list.get(0), null);
+    // if(list.get(0).getImgUrl().equals(name)){
+    // //Passed;
+    // }else fail();
+    // }
     @Test
     public void testQuery() {
         PaginationEntity page = new PaginationEntity();
         page.setOffset(0);
         page.setSize(10);
 
-        page.addOrderByEntity(new OrderByEntity("CREATE_TIME", DataaccessConstants.ORDER_DESC));
+        page.addOrderByEntity(new OrderByEntity("CREATE_TIME",
+                DataaccessConstants.ORDER_DESC));
 
         ClassPhotoEntityExt classPhotoEntityExt = new ClassPhotoEntityExt();
         classPhotoEntityExt.setTitle("_test_");
 
-        List<ClassPhotoEntityExt> result = classPhotoEntityExtMapper.list(classPhotoEntityExt, page);
+        List<ClassPhotoEntityExt> result = classPhotoEntityExtMapper.list(
+                classPhotoEntityExt, page);
         Assert.assertEquals(4, result.size());
-       Assert.assertEquals("_test_title_2_爱上课", result.get(1).getTitle());
+        Assert.assertEquals("_test_title_2_爱上课", result.get(1).getTitle());
     }
+
     @Test
     public void testQuery2() {
         ClassPhotoEntityExt classPhotoEntityExt = new ClassPhotoEntityExt();
@@ -191,10 +202,11 @@ public class ClassPhotoEntityExtTest extends BaseTest{
         idSet.add(2);
         idSet.add(3);
         classPhotoEntityExt.setIdSet(idSet);
-        List<ClassPhotoEntityExt> result = classPhotoEntityExtMapper.list(classPhotoEntityExt, page);
+        List<ClassPhotoEntityExt> result = classPhotoEntityExtMapper.list(
+                classPhotoEntityExt, page);
         Assert.assertEquals(3, result.size());
     }
-    
+
     @Test
     public void testQuery3() {
         ClassPhotoEntityExt classPhotoEntityExt = new ClassPhotoEntityExt();
@@ -202,12 +214,13 @@ public class ClassPhotoEntityExtTest extends BaseTest{
         page.setOffset(0);
         page.setSize(10);
         classPhotoEntityExt.setPartnerId(4);
-      
-        List<ClassPhotoEntityExt> result = classPhotoEntityExtMapper.list(classPhotoEntityExt, page);
+
+        List<ClassPhotoEntityExt> result = classPhotoEntityExtMapper.list(
+                classPhotoEntityExt, page);
         Assert.assertEquals(1, result.size());
         Assert.assertEquals("_test_IU_2_爱上课", result.get(0).getImgUrl());
     }
-    
+
     @Test
     public void testQuery4() {
         ClassPhotoEntityExt classPhotoEntityExt = new ClassPhotoEntityExt();
@@ -216,9 +229,48 @@ public class ClassPhotoEntityExtTest extends BaseTest{
         page.setSize(10);
         classPhotoEntityExt.setPartnerId(4);
         classPhotoEntityExt.setId(2);
-        List<ClassPhotoEntityExt> result = classPhotoEntityExtMapper.list(classPhotoEntityExt, page);
+        List<ClassPhotoEntityExt> result = classPhotoEntityExtMapper.list(
+                classPhotoEntityExt, page);
         Assert.assertEquals(0, result.size());
     }
+
+    @Test
+    public void testQuery5() {
+
+        int teacherCount = classPhotoEntityExtMapper.getCountByPartnerId(2);
+
+        Assert.assertSame(1, teacherCount);
+
+        List<ClassPhotoEntityExt> result = classPhotoEntityExtMapper
+                .listClassPhotoByPartnerId(5);
+        Assert.assertEquals(1, result.size());
+        Assert.assertEquals("_test_IU_1_爱上课", result.get(0).getImgUrl());
+    }
+
+    @Test
+    public void testQuery6() {
+
+        int teacherCount = classPhotoEntityExtMapper.getCountByCourseId(2);
+        ;
+        Assert.assertSame(1, teacherCount);
+
+        List<ClassPhotoEntityExt> result = classPhotoEntityExtMapper
+                .listClassPhotoByCourseId(2);
+        Assert.assertEquals(1, result.size());
+        Assert.assertEquals("_test_IU_1_爱上课", result.get(0).getImgUrl());
+    }
+
+    @Test
+    public void testQuery7() {
+
+        int teacherCount = classPhotoEntityExtMapper
+                .getCountByCourseTemplateId(2);
+        ;
+        Assert.assertSame(1, teacherCount);
+
+        List<ClassPhotoEntityExt> result = classPhotoEntityExtMapper
+                .listClassPhotoByCourseTempleteId(3);
+        Assert.assertEquals(1, result.size());
+        Assert.assertEquals("_test_IU_3_爱上课", result.get(0).getImgUrl());
+    }
 }
-
-
--- iShangkeProject/iShangkeProject-Dataaccess/src/test/java/com/ishangke/edunav/dataaccess/mapper/TeacherEntityExtTest.java
+++ iShangkeProject/iShangkeProject-Dataaccess/src/test/java/com/ishangke/edunav/dataaccess/mapper/TeacherEntityExtTest.java
@@ -22,14 +22,13 @@ import com.ishangke.edunav.dataaccess.model.TeacherEntityExt;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath*:applicationContext-dataaccessUT.xml" })
 @Transactional
-
 public class TeacherEntityExtTest {
 
     @Autowired
     private TeacherEntityExtMapper teacherEntityExtMapper;
-    
+
     @Test
-    public void testAdd(){
+    public void testAdd() {
         Calendar time = Calendar.getInstance();
         TeacherEntityExt teacherEntityExt = new TeacherEntityExt();
         teacherEntityExt.setCreateTime(time);
@@ -41,11 +40,11 @@ public class TeacherEntityExtTest {
         int oldCount = teacherEntityExtMapper.getCount();
         teacherEntityExtMapper.add(teacherEntityExt);
         Assert.assertSame(teacherEntityExtMapper.getCount(), oldCount + 1);
-        
+
     }
-    
+
     @Test
-    public void testDelete(){
+    public void testDelete() {
         Calendar time = Calendar.getInstance();
         TeacherEntityExt teacherEntityExt = new TeacherEntityExt();
         teacherEntityExt.setCreateTime(time);
@@ -53,83 +52,93 @@ public class TeacherEntityExtTest {
         teacherEntityExt.setDeleted(0);
         teacherEntityExt.setEnabled(1);
         teacherEntityExt.setPartnerId(1);
-        teacherEntityExt.setPopularity(1);       
+        teacherEntityExt.setPopularity(1);
         teacherEntityExtMapper.add(teacherEntityExt);
         int oldCount = teacherEntityExtMapper.getCount();
         teacherEntityExtMapper.deleteById(teacherEntityExt.getId());
         Assert.assertSame(teacherEntityExtMapper.getCount(), oldCount - 1);
     }
-    
-//    @Test
-//    public void testList(){
-//        Calendar time = Calendar.getInstance();
-//        TeacherEntityExt teacherEntityExt = new TeacherEntityExt();
-//        teacherEntityExt.setCreateTime(time);
-//        teacherEntityExt.setLastModifyTime(time);
-//        teacherEntityExt.setDeleted(0);
-//        teacherEntityExt.setEnabled(1);
-//        teacherEntityExt.setPartnerId(1);
-//        teacherEntityExt.setPopularity(1);       
-//        teacherEntityExtMapper.add(teacherEntityExt);
-////        teacherEntityExt.setCreateTimeStart(new Calendar(System.currentTimeMillis() - 10000));
-////        teacherEntityExt.setLastModifyTimeStart(new Calendar(System.currentTimeMillis() - 10000));
-//        
-//        List<TeacherEntityExt> list = teacherEntityExtMapper.list(teacherEntityExt, null);
-//        Assert.assertSame(list.size(),1);
-//        
-//        int listSize = teacherEntityExtMapper.getListCount(teacherEntityExt);
-//        Assert.assertSame(listSize,1);
-//        
-//        PaginationEntity page = new PaginationEntity();
-//        page.setOffset(0);
-//        page.setSize(1);
-//        
-//        list = teacherEntityExtMapper.list(teacherEntityExt, null);
-//        Assert.assertSame(list.size(),1);
-//        
-//        listSize = teacherEntityExtMapper.getListCount(teacherEntityExt);
-//        Assert.assertSame(listSize,1);
-//        
-//    }
-//    
-//    @Test
-//    public void testUpdate(){
-//        Calendar time = Calendar.getInstance();
-//        TeacherEntityExt teacherEntityExt = new TeacherEntityExt();
-//        teacherEntityExt.setCreateTime(time);
-//        teacherEntityExt.setLastModifyTime(time);
-//        teacherEntityExt.setDeleted(0);
-//        teacherEntityExt.setEnabled(1);
-//        teacherEntityExt.setPartnerId(1);
-//        teacherEntityExt.setPopularity(1);       
-//        teacherEntityExtMapper.add(teacherEntityExt);
-////        teacherEntityExt.setCreateTimeStart(new Calendar(System.currentTimeMillis() - 10000));
-////        teacherEntityExt.setLastModifyTimeStart(new Calendar(System.currentTimeMillis() - 10000));
-//        
-//        List<TeacherEntityExt> list = teacherEntityExtMapper.list(teacherEntityExt, null);
-//        String imgUrl = "李清";
-//        list.get(0).setImgUrl(imgUrl);
-//        teacherEntityExtMapper.update(list.get(0));
-//        list = teacherEntityExtMapper.list(list.get(0), null);
-//        
-//        if(list.get(0).getImgUrl().equals(imgUrl)){
-//            //Passed;
-//        }else fail();
-//    }
+
+    // @Test
+    // public void testList(){
+    // Calendar time = Calendar.getInstance();
+    // TeacherEntityExt teacherEntityExt = new TeacherEntityExt();
+    // teacherEntityExt.setCreateTime(time);
+    // teacherEntityExt.setLastModifyTime(time);
+    // teacherEntityExt.setDeleted(0);
+    // teacherEntityExt.setEnabled(1);
+    // teacherEntityExt.setPartnerId(1);
+    // teacherEntityExt.setPopularity(1);
+    // teacherEntityExtMapper.add(teacherEntityExt);
+    // // teacherEntityExt.setCreateTimeStart(new
+    // Calendar(System.currentTimeMillis() - 10000));
+    // // teacherEntityExt.setLastModifyTimeStart(new
+    // Calendar(System.currentTimeMillis() - 10000));
+    //
+    // List<TeacherEntityExt> list =
+    // teacherEntityExtMapper.list(teacherEntityExt, null);
+    // Assert.assertSame(list.size(),1);
+    //
+    // int listSize = teacherEntityExtMapper.getListCount(teacherEntityExt);
+    // Assert.assertSame(listSize,1);
+    //
+    // PaginationEntity page = new PaginationEntity();
+    // page.setOffset(0);
+    // page.setSize(1);
+    //
+    // list = teacherEntityExtMapper.list(teacherEntityExt, null);
+    // Assert.assertSame(list.size(),1);
+    //
+    // listSize = teacherEntityExtMapper.getListCount(teacherEntityExt);
+    // Assert.assertSame(listSize,1);
+    //
+    // }
+    //
+    // @Test
+    // public void testUpdate(){
+    // Calendar time = Calendar.getInstance();
+    // TeacherEntityExt teacherEntityExt = new TeacherEntityExt();
+    // teacherEntityExt.setCreateTime(time);
+    // teacherEntityExt.setLastModifyTime(time);
+    // teacherEntityExt.setDeleted(0);
+    // teacherEntityExt.setEnabled(1);
+    // teacherEntityExt.setPartnerId(1);
+    // teacherEntityExt.setPopularity(1);
+    // teacherEntityExtMapper.add(teacherEntityExt);
+    // // teacherEntityExt.setCreateTimeStart(new
+    // Calendar(System.currentTimeMillis() - 10000));
+    // // teacherEntityExt.setLastModifyTimeStart(new
+    // Calendar(System.currentTimeMillis() - 10000));
+    //
+    // List<TeacherEntityExt> list =
+    // teacherEntityExtMapper.list(teacherEntityExt, null);
+    // String imgUrl = "李清";
+    // list.get(0).setImgUrl(imgUrl);
+    // teacherEntityExtMapper.update(list.get(0));
+    // list = teacherEntityExtMapper.list(list.get(0), null);
+    //
+    // if(list.get(0).getImgUrl().equals(imgUrl)){
+    // //Passed;
+    // }else fail();
+    // }
     @Test
     public void testQuery() {
         PaginationEntity page = new PaginationEntity();
         page.setOffset(0);
         page.setSize(10);
-        //排序，先按照第一个排序，再按照第二个排序，依次排列
-        page.addOrderByEntity(new OrderByEntity("CREATE_TIME", DataaccessConstants.ORDER_DESC));
-        page.addOrderByEntity(new OrderByEntity("LAST_MODIFY_TIME", DataaccessConstants.ORDER_DESC));
+        // 排序，先按照第一个排序，再按照第二个排序，依次排列
+        page.addOrderByEntity(new OrderByEntity("CREATE_TIME",
+                DataaccessConstants.ORDER_DESC));
+        page.addOrderByEntity(new OrderByEntity("LAST_MODIFY_TIME",
+                DataaccessConstants.ORDER_DESC));
         TeacherEntityExt teacherEntityExt = new TeacherEntityExt();
         teacherEntityExt.setName("_test_");
-        List<TeacherEntityExt> result = teacherEntityExtMapper.list(teacherEntityExt, page);
+        List<TeacherEntityExt> result = teacherEntityExtMapper.list(
+                teacherEntityExt, page);
         Assert.assertEquals(4, result.size());
         Assert.assertEquals("_test_name_1_爱上课", result.get(0).getName());
     }
+
     @Test
     public void testQuery2() {
         TeacherEntityExt TeacherEntityExt = new TeacherEntityExt();
@@ -141,10 +150,11 @@ public class TeacherEntityExtTest {
         idSet.add(2);
         idSet.add(3);
         TeacherEntityExt.setIdSet(idSet);
-        List<TeacherEntityExt> result = teacherEntityExtMapper.list(TeacherEntityExt, page);
+        List<TeacherEntityExt> result = teacherEntityExtMapper.list(
+                TeacherEntityExt, page);
         Assert.assertEquals(3, result.size());
     }
-    
+
     @Test
     public void testQuery3() {
         TeacherEntityExt TeacherEntityExt = new TeacherEntityExt();
@@ -152,13 +162,15 @@ public class TeacherEntityExtTest {
         page.setOffset(0);
         page.setSize(10);
         TeacherEntityExt.setEnabled(2);
-        page.addOrderByEntity(new OrderByEntity("POPULARITY", DataaccessConstants.ORDER_DESC));
-        List<TeacherEntityExt> result = teacherEntityExtMapper.list(TeacherEntityExt, page);
+        page.addOrderByEntity(new OrderByEntity("POPULARITY",
+                DataaccessConstants.ORDER_DESC));
+        List<TeacherEntityExt> result = teacherEntityExtMapper.list(
+                TeacherEntityExt, page);
         Assert.assertEquals(2, result.size());
         Assert.assertEquals("_test_name_2_爱上课", result.get(0).getName());
         Assert.assertEquals("_test_name_2_爱上课", result.get(0).getName());
     }
-    
+
     @Test
     public void testQuery4() {
         TeacherEntityExt TeacherEntityExt = new TeacherEntityExt();
@@ -167,8 +179,45 @@ public class TeacherEntityExtTest {
         page.setSize(10);
         TeacherEntityExt.setEnabled(2);
         TeacherEntityExt.setPartnerId(2);
-        List<TeacherEntityExt> result = teacherEntityExtMapper.list(TeacherEntityExt, page);
+        List<TeacherEntityExt> result = teacherEntityExtMapper.list(
+                TeacherEntityExt, page);
         Assert.assertEquals(1, result.size());
         Assert.assertEquals("_test_name_2_爱上课", result.get(0).getName());
+    }
+
+    @Test
+    public void testQuery5() {
+
+        int teacherCount = teacherEntityExtMapper.getCountByPartnerId(3);
+        Assert.assertSame(1, teacherCount);
+
+        List<TeacherEntityExt> result = teacherEntityExtMapper
+                .listTeacherByPartnerId(4);
+        Assert.assertEquals(1, result.size());
+        Assert.assertEquals("_test_name_2_爱上课", result.get(0).getName());
+    }
+
+    @Test
+    public void testQuery6() {
+        int teacherCount = teacherEntityExtMapper.getCountByCourseId(3);
+        Assert.assertSame(1, teacherCount);
+
+        List<TeacherEntityExt> result = teacherEntityExtMapper
+                .listTeacherByCourseId(4);
+        Assert.assertEquals(1, result.size());
+        Assert.assertEquals("_test_name_2_爱上课", result.get(0).getName());
+    }
+
+    @Test
+    public void testQuery7() {
+
+        int teacherCount = teacherEntityExtMapper.getCountByCourseTemplateId(2);
+        ;
+        Assert.assertSame(1, teacherCount);
+
+        List<TeacherEntityExt> result = teacherEntityExtMapper
+                .listTeacherByCourseTempleteId(5);
+        Assert.assertEquals(1, result.size());
+        Assert.assertEquals("_test_name_1_爱上课", result.get(0).getName());
     }
 }
