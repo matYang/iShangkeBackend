e4eced6fa0c7717cbbf60187109749a2926560f9
Aaron <467517168@qq.com>
2014-07-26 15:55:05 +0800

    course 业务逻辑

--- iShangkeProject/iShangkeProject-Service/src/main/java/com/ishangke/edunav/manager/CourseManager.java
+++ iShangkeProject/iShangkeProject-Service/src/main/java/com/ishangke/edunav/manager/CourseManager.java
@@ -3,13 +3,10 @@ package com.ishangke.edunav.manager;
 import java.util.List;
 
 import com.ishangke.edunav.commoncontract.model.CategoryBo;
-import com.ishangke.edunav.commoncontract.model.CircleBo;
-import com.ishangke.edunav.commoncontract.model.CommentCourseApproveBo;
-import com.ishangke.edunav.commoncontract.model.CommentCourseBo;
-import com.ishangke.edunav.commoncontract.model.CommentCourseRejectBo;
+import com.ishangke.edunav.commoncontract.model.CourseCommentBo;
 import com.ishangke.edunav.commoncontract.model.CourseBo;
+import com.ishangke.edunav.commoncontract.model.CourseCommentBo;
 import com.ishangke.edunav.commoncontract.model.CourseTemplateBo;
-import com.ishangke.edunav.commoncontract.model.LocationBo;
 import com.ishangke.edunav.commoncontract.model.PaginationBo;
 import com.ishangke.edunav.commoncontract.model.PartnerBo;
 import com.ishangke.edunav.commoncontract.model.UserBo;
@@ -43,174 +40,43 @@ public interface CourseManager {
     CourseBo createCourse(CourseTemplateBo courseTemplateBo, CourseBo courseBo, PartnerBo partnerBo, UserBo userBo);
 
     /**
-     * 本方法为提交课程<br>
-     * 合作商管理员可以通过此方法提交课程
-     * 
-     * @param courseBo
-     *            需要被创建的课程信息
-     * @param partnerBo
-     *            合作商信息
-     * @param userBo
-     *            调用方法的用户的信息
-     * 
-     * @return
-     * 
-     * 
-     * @param courseBo
-     * @param partnerBo
-     * @param userBo
-     */
-    CourseBo submitCourse(CourseBo courseBo, PartnerBo partnerBo, UserBo userBo);
-
-    /**
-     * 本方法为审核通过课程<br>
-     * ishangke管理员可以通过此方法审核通过课程
-     * 
-     * @param courseBo
-     *            需要被审核的课程信息
-     * @param partnerBo
-     *            合作商信息
-     * @param commentCourseApproveBo   $
-     *            审核通过课程评论信息
-     * @param userBo
-     *            调用方法的用户的信息
-     * 
-     * @return
-     * 
-     * 
-     * @param courseBo
-     * @param partnerBo
-     * @param commentCourseApproveBo
-     * @param userBo
-     */
-    CourseBo approveCourse(CourseBo courseBo, PartnerBo partnerBo, CommentCourseApproveBo commentCourseApproveBo, UserBo userBo);
-
-    /**
-     * 本方法为审核不通过课程<br>
-     * ishangke管理员可以通过此方法审核不通过课程
-     * 
-     * @param courseBo
-     *            需要被审核的课程信息
-     * @param partnerBo
-     *            合作商信息
-     * @param commentCourseApproveBo  $
-     *            审核不通过课程评论信息
-     * @param userBo
-     *            调用方法的用户的信息
-     * 
-     * @return
-     * 
-     * 
-     * @param courseBo
-     * @param partnerBo
-     * @param commentCourseRejectBo
-     * @param userBo
-     */
-    CourseBo rejectCourse(CourseBo courseBo, PartnerBo partnerBo, CommentCourseRejectBo commentCourseRejectBo, UserBo userBo);
-
-    /**
-     * 本方法为下架课程<br>
-     * 合作商管理员可以通过此方法下架课程
-     * 
-     * @param courseBo
-     *            需要被下架的课程信息
-     * @param partnerBo
-     *            合作商信息
-     * @param userBo
-     *            调用方法的用户的信息
-     * 
-     * @return
-     * 
-     * 
-     * @param courseBo
-     * @param partnerBo
-     * @param userBo
-     */
-    CourseBo cancelCourse(CourseBo courseBo, PartnerBo partnerBo, UserBo userBo);
-
-    /**
-     * 本方法为重新上架课程<br>
-     * 合作商管理员可以通过此方法重新上架课程
-     * 
-     * @param courseBo
-     *            需要被重新上架的课程信息
-     * @param partnerBo
-     *            合作商信息
-     * @param userBo
-     *            调用方法的用户的信息
-     * 
-     * @return
-     * 
-     * 
-     * @param courseBo
-     * @param partnerBo
-     * @param userBo
-     */
-    CourseBo resubmitCourse(CourseBo courseBo, PartnerBo partnerBo, UserBo userBo);
-
-    /**
-     * 本方法为删除课程<br>
-     * 合作商管理员可以通过此方法删除已经下架的课程
-     * 
-     * @param courseeBo
-     *            需要被删除的课程信息
-     * @param partnerBo
-     *            合作商信息
-     * @param userBo
-     *            调用方法的用户的信息
-     * 
-     * @return
-     * 
-     * 
-     * @param courseBo
-     * @param partnerBo
-     * @param userBo
-     */
-    CourseBo deleteCourse(CourseBo courseBo, PartnerBo partnerBo, UserBo userBo);
-
-    /**
-     * 本方法为修改课程<br>
-     * 合作商管理员可以通过此方法修改课程
-     * 
-     * @param courseBo
-     *            需要被修改的课程信息
-     * @param partnerBo
-     *            合作商信息
-     * @param userBo
-     *            调用方法的用户的信息
-     * 
-     * @return
-     * 
-     * 
-     * @param courseBo
-     * @param partnerBo
-     * @param userBo
-     */
-    CourseBo updateCourse(CourseBo courseBo, PartnerBo partnerBo, UserBo userBo);
-
-    /**
      * 本方法为给课程评论<br>
      * 用户可以通过此方法给课程进行评论
      * 
      * @param courseBo
      *            需要被评论的课程信息
-     * @param commentCourseBo   $
+     * @param CourseCommentBo   $
      *            课程评论信息
-     * @param partnerBo
-     *            合作商信息
      * @param userBo
      *            调用方法的用户的信息
      * 
-     * @return 课程评论实体 CommentCourseBo
+     * @return 课程评论实体 CourseCommentBo
      * 
      * 
      * @param courseBo
-     * @param commentCourseBo
+     * @param CourseCommentBo
      * @param partnerBo
      * @param userBo
      */
-    CourseBo commentCourse(CourseBo courseBo, CommentCourseBo commentCourseBo, PartnerBo partnerBo, UserBo userBo);
+    CourseCommentBo commentCourse(CourseBo courseBo, CourseCommentBo courseCommnet, UserBo userBo);
 
+    /**
+     * 查询课程的评价
+     * @param courseBo
+     * @return
+     */
+    List<CourseCommentBo> queryCommentBuCourseId(CourseBo courseBo, PaginationBo paginationBo);
+    
+    /**
+     * 删除课程的评价
+     * @param CourseCommentBo
+     * @param userBo
+     * @return
+     */
+    CourseCommentBo deleteCommentByCommentId(CourseCommentBo courseCommentBo, UserBo userBo);
+    
+    
+    
     /**
      * 本方法为根据关键字查找课程<br>
      * 通过此方法可以直接过滤搜索出按Category查找的课程
@@ -218,12 +84,12 @@ public interface CourseManager {
      * @param String
      *            CategoryBo关键字
      * 
-     * @return 课程实体 CourseBo 列表
+     * @return CategoryBo 列表
      * 
      * 
      * @param keyword
      */
-    List<CourseBo> queryByKeyword(String keyword);
+    List<CategoryBo> queryByKeyword(String keyword);
 
     /**
      * 本方法为合作商提供查询课程的功能。<br>
@@ -251,15 +117,10 @@ public interface CourseManager {
     /**
      * 本方法为用户查询课程的功能。<br>
      * 用户可以按照category，location，courseFilter查询所有课程
+     * 只能搜索已上线的课程
      * 
      * @param courseBo
      *            course过滤信息
-     * @param categoryBo
-     *            category过滤信息
-     * @param locationBo
-     *            location过滤信息
-     * @param circleBo
-     *            circle过滤信息
      * @param paginationBo
      *            分页信息
      * 
@@ -267,12 +128,9 @@ public interface CourseManager {
      * 
      * 
      * @param courseBo
-     * @param categoryBo
-     * @param locationBo
-     * @param circleBo
      * @param paginationBo
      */
-    List<CourseBo> queryByFilter(CourseBo courseBo, CategoryBo categoryBo, LocationBo locationBo, CircleBo circleBo, PaginationBo paginationBo);
+    List<CourseBo> queryByFilter(CourseBo courseBo, PaginationBo paginationBo);
 
     /**
      * 本方法返回课程详情<br>
@@ -290,5 +148,13 @@ public interface CourseManager {
      * @param userBo
      */
     CourseBo queryById(CourseBo courseBo, UserBo userBo);
-
+    
+    /**
+     * 修改course状态
+     * @param courseBo
+     * @param operation
+     * @param userBo
+     * @return
+     */
+    CourseBo transformCourseStatus(CourseBo courseBo, int operation, UserBo userBo);
 }
