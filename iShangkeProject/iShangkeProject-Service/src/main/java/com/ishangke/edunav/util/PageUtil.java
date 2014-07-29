fd007ce5e938e7592eb9d24859c11f2e24ba4075
matYang <uwse@me.com>
2014-07-29 15:08:01 +0800

    sett max

--- iShangkeProject/iShangkeProject-Service/src/main/java/com/ishangke/edunav/util/PageUtil.java
+++ iShangkeProject/iShangkeProject-Service/src/main/java/com/ishangke/edunav/util/PageUtil.java
@@ -5,7 +5,7 @@ import com.ishangke.edunav.commoncontract.model.PaginationBo;
 public class PageUtil {
     
     private static final int OFFSET = 0;
-    private static final int SIZE = 10;
+    private static final int SIZE = Integer.MAX_VALUE;
     
     public static PaginationBo getPage(PaginationBo page) {
         if (page == null) {
