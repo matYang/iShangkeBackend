509c611600b3f7a0f7ab2b973a559ca3679301c8
liqing0216 <1670990633@qq.com>
2014-07-30 19:55:45 +0800

    facade test do not use junti

--- iShangkeProject/iShangkeProject-UserWeb/src/main/java/com/ishangke/edunav/web/user/test/CourseTest.java
+++ iShangkeProject/iShangkeProject-UserWeb/src/main/java/com/ishangke/edunav/web/user/test/CourseTest.java
@@ -1,10 +1,11 @@
-package com.ishangke.edunav.web.user.vo;
+package com.ishangke.edunav.web.user.test;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
 
 import com.ishangke.edunav.common.utilities.DateUtility;
 import com.ishangke.edunav.commoncontract.model.CourseBo;
