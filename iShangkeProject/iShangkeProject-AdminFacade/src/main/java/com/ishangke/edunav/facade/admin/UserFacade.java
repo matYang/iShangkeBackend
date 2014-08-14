9e18400db0ade750c2ece3b756f57a413058ddb6
Aaron <467517168@qq.com>
2014-08-14 10:57:02 +0800

    增加对query 过滤时的条件增强

--- iShangkeProject/iShangkeProject-AdminFacade/src/main/java/com/ishangke/edunav/facade/admin/UserFacade.java
+++ iShangkeProject/iShangkeProject-AdminFacade/src/main/java/com/ishangke/edunav/facade/admin/UserFacade.java
@@ -173,6 +173,24 @@ public class UserFacade {
 
         return result;
     }
+    
+    public UserPageViewBo queryUserByPartnerIdAndRoleId(int partnerId, int roleId, UserBo currentUser, PaginationBo paginationBo, String permissionTag) {
+        UserPageViewBo result = null;
+
+        ThriftClientSetting clientSetting = ThriftClientSettingManager.getSetting(ClientEnum.User.getName());
+
+        try (ThriftClientFactory<UserService.Client> factory = new ThriftClientFactory<>(clientSetting)) {
+            Client serviceClient = factory.getServiceClient();
+            result = serviceClient.queryUserByPartnerIdAndRoleId(partnerId, roleId, currentUser, paginationBo, PermissionCache.getTag(permissionTag));
+        } catch (BusinessExceptionBo e) {
+            e.printStackTrace();
+            throw new ControllerException(e.getErrorCode(), e.getMessageKey(), e.getMessage());
+        } catch (TException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
 
     public UserBo authenticate(SessionBo sessionBo, String permissionTag) {
         UserBo result = null;
