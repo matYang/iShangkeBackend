9aec2190afa7afe88c4b7bcb5c3ef3822f99c4bd
Aaron <467517168@qq.com>
2014-08-26 02:32:29 +0800

    不玩了。。。

--- iShangkeProject/iShangkeProject-UserWeb/src/main/java/com/ishangke/edunav/web/user/controller/course/CourseController.java
+++ iShangkeProject/iShangkeProject-UserWeb/src/main/java/com/ishangke/edunav/web/user/controller/course/CourseController.java
@@ -70,8 +70,7 @@ public class CourseController extends AbstractController{
         pageViewVo = CoursePageViewConverter.toModel(pageViewBo);
         String category = courseVo.getCategoryValue() == null ? "" : courseVo.getCategoryValue();
         String idSet = courseVo.getIdSet() == null ? "" : courseVo.getIdSet().toString();
-        //加入cookie 便于日后统计
-        LOGGER.info(req.getCookies().toString() + "[queryCourse]:category:" + category + ";idSet:" + idSet + ";ColumnKey:" + columnKey + ";Order:" + order);
+        LOGGER.info("[queryCourse]:category:" + category + ";idSet:" + idSet + ";ColumnKey:" + columnKey + ";Order:" + order);
         return pageViewVo;
     }
     
