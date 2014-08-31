57d1cca80d6cc3c950899cf24eaf6aa4020353dc
Aaron <467517168@qq.com>
2014-08-31 23:26:46 +0800

    修改短信内容；修改返现券金额；匿名用户创建booking的时候也可以使返现券

--- iShangkeProject/iShangkeProject-Common/src/main/java/com/ishangke/edunav/common/constant/DefaultValue.java
+++ iShangkeProject/iShangkeProject-Common/src/main/java/com/ishangke/edunav/common/constant/DefaultValue.java
@@ -4,7 +4,7 @@ public class DefaultValue {
 
         public static final double DOUBLEPRCISIONOFFSET = 0.001d;
         
-        public static final double COUPONREGISTRATIONVALUE = 50.0d;
+        public static final double COUPONREGISTRATIONVALUE = 800.0d;
         
         public static final double COUPONINVITATIONVALUE = 20.0d;
 }
