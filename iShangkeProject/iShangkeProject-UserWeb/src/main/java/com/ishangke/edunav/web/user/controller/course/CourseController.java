4676cb7f2f1ffe8a7cf02927615bf732d9a1fb93
Aaron <467517168@qq.com>
2014-09-04 11:28:03 +0800

    haha forbidden api directly

--- iShangkeProject/iShangkeProject-UserWeb/src/main/java/com/ishangke/edunav/web/user/controller/course/CourseController.java
+++ iShangkeProject/iShangkeProject-UserWeb/src/main/java/com/ishangke/edunav/web/user/controller/course/CourseController.java
@@ -43,6 +43,10 @@ public class CourseController extends AbstractController {
     @RequestMapping(value = "", method = RequestMethod.GET, produces = "application/json")
     public @ResponseBody
     JsonResponse queryCourse(CourseVo courseVo, PaginationVo paginationVo, HttpServletRequest req, HttpServletResponse resp) {
+        if (req.getHeader("referer") == null && !req.getHeader("referer").contains("ishangke.cn")) {
+            JsonResponse err = new JsonResponse();
+            err.setMessage("sorry~ you can not query our course by api directly~");
+        }
         String permissionTag = this.getUrl(req);
 
         CoursePageViewBo pageViewBo = null;
@@ -81,7 +85,10 @@ public class CourseController extends AbstractController {
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = "application/json")
     public @ResponseBody
     JsonResponse queryCourseById(@PathVariable("id") int id, HttpServletRequest req, HttpServletResponse resp) {
-        LOGGER.warn("referer:" + req.getHeader("referer"));
+        if (req.getHeader("referer") == null && !req.getHeader("referer").contains("ishangke.cn")) {
+            JsonResponse err = new JsonResponse();
+            err.setMessage("sorry~ you can not query our course by api directly~");
+        }
         String permissionTag = this.getUrl(req);
 
         CourseVo courseVo = new CourseVo();
