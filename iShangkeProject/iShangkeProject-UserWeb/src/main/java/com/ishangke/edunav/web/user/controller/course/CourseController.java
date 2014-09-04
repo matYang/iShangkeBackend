5ac947f6601b21dbabe1a17541e5c4c1a64f6936
Aaron <467517168@qq.com>
2014-09-04 09:51:13 +0800

    test 禁止别人copy我们的课程

--- iShangkeProject/iShangkeProject-UserWeb/src/main/java/com/ishangke/edunav/web/user/controller/course/CourseController.java
+++ iShangkeProject/iShangkeProject-UserWeb/src/main/java/com/ishangke/edunav/web/user/controller/course/CourseController.java
@@ -83,6 +83,7 @@ public class CourseController extends AbstractController{
     
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = "application/json")
     public @ResponseBody JsonResponse  queryCourseById(@PathVariable("id") int id, HttpServletRequest req, HttpServletResponse resp) {
+        LOGGER.warn("host:" + req.getServerName());
         String permissionTag = this.getUrl(req);
         
         CourseVo courseVo = new CourseVo();
