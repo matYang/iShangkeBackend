e924d631cf78d69a4e20a8827472b51b4c991895
Aaron <467517168@qq.com>
2014-09-10 22:05:57 +0800

    利用开课日期下线课程

--- iShangkeProject/iShangkeProject-Service/src/main/java/com/ishangke/edunav/clean/CleanEventJob.java
+++ iShangkeProject/iShangkeProject-Service/src/main/java/com/ishangke/edunav/clean/CleanEventJob.java
@@ -104,12 +104,12 @@ public class CleanEventJob {
         CourseEntityExt course = new CourseEntityExt();
         // online courses that and a cutoff date time <= current time
         course.setStatus(Constant.COURSESTATUSONLINED);
-        course.setCutoffDateEnd(DateUtility.getCurTimeInstance());
+        course.setStartDateEnd(DateUtility.getCurTimeInstance());
         // 为null的不能删除
         Calendar c = Calendar.getInstance();
         // 公元1年1月1号
         c.set(1, 0, 1, 0, 0, 0);
-        course.setCutoffDateStart(c);
+        course.setStartDateStart(c);
         try {
             List<CourseEntityExt> results = courseMapper.list(course, getDefaultPagination());
             if (results != null) {
