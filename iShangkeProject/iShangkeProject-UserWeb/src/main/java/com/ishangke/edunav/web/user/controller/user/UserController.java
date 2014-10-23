cd928e39a06dad86e901594cf468a129a70023a1
Aaron <467517168@qq.com>
2014-10-23 11:44:39 +0800

    卧槽 居然有人攻击我们

--- iShangkeProject/iShangkeProject-UserWeb/src/main/java/com/ishangke/edunav/web/user/controller/user/UserController.java
+++ iShangkeProject/iShangkeProject-UserWeb/src/main/java/com/ishangke/edunav/web/user/controller/user/UserController.java
@@ -3,6 +3,8 @@ package com.ishangke.edunav.web.user.controller.user;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,28 +33,30 @@ import com.ishangke.edunav.web.user.controller.AbstractController;
 
 @Controller
 @RequestMapping("/api/v2/user")
-public class UserController extends AbstractController{
-    
+public class UserController extends AbstractController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     UserFacade userFacade;
-    
+
     @RequestMapping(value = "/login/phone", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
-    public @ResponseBody JsonResponse login(@RequestBody LoginVo loginVo, HttpServletRequest req, HttpServletResponse resp) {
+    public @ResponseBody
+    JsonResponse login(@RequestBody LoginVo loginVo, HttpServletRequest req, HttpServletResponse resp) {
         UserVo responseVo = null;
-        
+
         String permissionTag = this.getUrl(req);
         boolean remember = (loginVo.getRemember() != null && loginVo.getRemember() == 1);
-        
+
         SessionBo authSessionBo = this.getSession(req);
         boolean loggedIn;
-        
+
         if (loginVo.getAccountIdentifier() == null || loginVo.getAccountIdentifier().length() == 0) {
             return this.handleWebException(new ControllerException("请输入账号"), resp);
         }
         if (loginVo.getPassword() == null || loginVo.getPassword().length() == 0) {
             return this.handleWebException(new ControllerException("请输入密码"), resp);
         }
-        
+
         UserBo curUser = null;
         try {
             loggedIn = userFacade.authenticate(authSessionBo, permissionTag).getId() > 0;
@@ -64,17 +68,18 @@ public class UserController extends AbstractController{
                 this.openSession(authSessionBo, remember, req, resp);
             }
 
-            curUser = userFacade.getCurrentUser(authSessionBo, permissionTag);    
+            curUser = userFacade.getCurrentUser(authSessionBo, permissionTag);
         } catch (ControllerException c) {
             return this.handleWebException(c, resp);
         }
-        
+
         responseVo = UserConverter.toModel(curUser);
         return responseVo;
     }
 
     @RequestMapping(value = "/qlogin", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
-    public @ResponseBody JsonResponse qlogin(@RequestBody LoginVo loginVo, HttpServletRequest req, HttpServletResponse resp) {
+    public @ResponseBody
+    JsonResponse qlogin(@RequestBody LoginVo loginVo, HttpServletRequest req, HttpServletResponse resp) {
         UserVo responseVo = null;
         if (loginVo.getAccountIdentifier() == null || loginVo.getAccountIdentifier().length() == 0) {
             return this.handleWebException(new ControllerException("请输入账号"), resp);
@@ -96,21 +101,22 @@ public class UserController extends AbstractController{
                 this.openSession(authSessionBo, false, req, resp);
             }
 
-            curUser = userFacade.getCurrentUser(authSessionBo, permissionTag);    
+            curUser = userFacade.getCurrentUser(authSessionBo, permissionTag);
         } catch (ControllerException c) {
             return this.handleWebException(c, resp);
         }
-        
+
         responseVo = UserConverter.toModel(curUser);
         return responseVo;
     }
-    
+
     @RequestMapping(value = "/{id}/logout", method = RequestMethod.PUT, consumes = "application/json", produces = "application/json")
-    public @ResponseBody JsonResponse logout(HttpServletRequest req, HttpServletResponse resp) {
+    public @ResponseBody
+    JsonResponse logout(HttpServletRequest req, HttpServletResponse resp) {
         String permissionTag = this.getUrl(req);
         SessionBo authSessionBo = this.getSession(req);
         if (authSessionBo.getId() <= 0) {
-            //already logged out
+            // already logged out
             return new EmptyResponse();
         }
         try {
@@ -119,46 +125,55 @@ public class UserController extends AbstractController{
             return this.handleWebException(c, resp);
         }
         this.closeSession(req, resp);
-        
+
         return new EmptyResponse();
     }
-    
-    
+
     @RequestMapping(value = "/findSession", method = RequestMethod.GET, produces = "application/json")
-    public @ResponseBody JsonResponse  findSession(HttpServletRequest req, HttpServletResponse resp) {
+    public @ResponseBody
+    JsonResponse findSession(HttpServletRequest req, HttpServletResponse resp) {
         UserVo responseVo = null;
-        
+
         String permissionTag = this.getUrl(req);
         SessionBo authSessionBo = this.getSession(req);
-        
+
         UserBo userBo = null;
         try {
-             userBo = userFacade.authenticate(authSessionBo, permissionTag);
+            userBo = userFacade.authenticate(authSessionBo, permissionTag);
         } catch (ControllerException c) {
             return this.handleWebException(c, resp);
-        } 
-        
+        }
+
         if (userBo.getId() <= 0) {
             responseVo = new UserVo();
             responseVo.setId(-1);
-        }
-        else {
+        } else {
             try {
                 userBo = userFacade.getCurrentUser(authSessionBo, permissionTag);
             } catch (ControllerException c) {
                 return this.handleWebException(c, resp);
-            } 
+            }
             responseVo = UserConverter.toModel(userBo);
         }
-        
+
         return responseVo;
     }
-    
-    
+
     @RequestMapping(value = "/smsVerification", method = RequestMethod.GET, produces = "application/json")
-    public @ResponseBody JsonResponse cv( @RequestParam(value="phone") String phone, HttpServletRequest req, HttpServletResponse resp) {
+    public @ResponseBody
+    JsonResponse cv(@RequestParam(value = "phone") String phone, HttpServletRequest req, HttpServletResponse resp) {
         String permissionTag = this.getUrl(req);
-        
+        String ip = req.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = req.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = req.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = req.getRemoteAddr();
+        }
+        LOGGER.error("ip:" + ip);
         UserVo userVo = new UserVo();
         userVo.setPhone(phone);
         try {
@@ -168,11 +183,12 @@ public class UserController extends AbstractController{
         }
         return new EmptyResponse();
     }
-    
+
     @RequestMapping(value = "/qloginSmsVerification", method = RequestMethod.GET, produces = "application/json")
-    public @ResponseBody JsonResponse qlogin( @RequestParam(value="phone") String phone, HttpServletRequest req, HttpServletResponse resp) {
+    public @ResponseBody
+    JsonResponse qlogin(@RequestParam(value = "phone") String phone, HttpServletRequest req, HttpServletResponse resp) {
         String permissionTag = this.getUrl(req);
-        
+
         UserVo userVo = new UserVo();
         userVo.setPhone(phone);
         try {
@@ -182,21 +198,22 @@ public class UserController extends AbstractController{
         }
         return new EmptyResponse();
     }
-    
+
     @RequestMapping(value = "/registration", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
-    public @ResponseBody JsonResponse register(@RequestBody UserVo userVo, HttpServletRequest req, HttpServletResponse resp) {
+    public @ResponseBody
+    JsonResponse register(@RequestBody UserVo userVo, HttpServletRequest req, HttpServletResponse resp) {
         UserVo responseVo = null;
-        
+
         String permissionTag = this.getUrl(req);
         SessionBo authSessionBo = this.getSession(req);
-        
+
         boolean loggedIn;
         try {
             loggedIn = userFacade.authenticate(authSessionBo, permissionTag).getId() > 0;
         } catch (ControllerException c) {
             return this.handleWebException(c, resp);
-        } 
-        
+        }
+
         if (loggedIn) {
             return this.handleWebException(new ControllerException("请先登出之前的账号"), resp);
         }
@@ -210,24 +227,27 @@ public class UserController extends AbstractController{
         if (!userVo.getPassword().equals(userVo.getConfirmPassword())) {
             return this.handleWebException(new ControllerException("两次输入密码不匹配"), resp);
         }
-        //invitation code actually stands for userName, where name is the real name to be filled after registration
-//        if (userVo.getInvitationCode() == null || userVo.getInvitationCode().length() == 0) {
-//            return this.handleWebException(new ControllerException("用户名不能为空"), resp);
-//        }
+        // invitation code actually stands for userName, where name is the real
+        // name to be filled after registration
+        // if (userVo.getInvitationCode() == null ||
+        // userVo.getInvitationCode().length() == 0) {
+        // return this.handleWebException(new ControllerException("用户名不能为空"),
+        // resp);
+        // }
         if (userVo.getAuthCode() == null || userVo.getAuthCode().length() == 0) {
             return this.handleWebException(new ControllerException("验证码不能为空"), resp);
         }
-        
+
         SessionVo cellSessionVo = new SessionVo();
         cellSessionVo.setAccountIdentifier(userVo.getPhone());
         cellSessionVo.setAuthCode(userVo.getAuthCode());
-        
+
         UserBo userBo = UserConverter.fromModel(userVo);
         SessionBo cellSessionBo = SessionConverter.fromModel(cellSessionVo);
 
         UserBo result = null;
         try {
-            result = userFacade.registerUser(userBo, cellSessionBo, permissionTag); 
+            result = userFacade.registerUser(userBo, cellSessionBo, permissionTag);
         } catch (ControllerException c) {
             return this.handleWebException(c, resp);
         }
@@ -240,44 +260,43 @@ public class UserController extends AbstractController{
         newSession.setAccountIdentifier(result.getPhone());
         newSession.setAuthCode(result.getAuthCode());
         this.openSession(newSession, false, req, resp);
-        
+
         responseVo = UserConverter.toModel(result);
         return responseVo;
     }
-    
-    
+
     @RequestMapping(value = "/forgetPassword", method = RequestMethod.GET, produces = "application/json")
-    public @ResponseBody JsonResponse fp(@RequestParam(value="phone") String phone, HttpServletRequest req, HttpServletResponse resp) {
+    public @ResponseBody
+    JsonResponse fp(@RequestParam(value = "phone") String phone, HttpServletRequest req, HttpServletResponse resp) {
         String permissionTag = this.getUrl(req);
-        
+
         UserVo userVo = new UserVo();
         userVo.setPhone(phone);
-        
+
         try {
             userFacade.openForgetPasswordSession(UserConverter.fromModel(userVo), permissionTag);
         } catch (ControllerException c) {
             return this.handleWebException(c, resp);
-        } 
-        
-        
+        }
+
         return new EmptyResponse();
     }
-    
-    
+
     @RequestMapping(value = "/forgetPassword", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
-    public @ResponseBody JsonResponse recoverPassword(@RequestBody PasswordVo passwordVo, HttpServletRequest req, HttpServletResponse resp) {
+    public @ResponseBody
+    JsonResponse recoverPassword(@RequestBody PasswordVo passwordVo, HttpServletRequest req, HttpServletResponse resp) {
         UserVo responseVo = null;
-        
+
         String permissionTag = this.getUrl(req);
         SessionBo authSessionBo = this.getSession(req);
-        
+
         boolean loggedIn;
         try {
             loggedIn = userFacade.authenticate(authSessionBo, permissionTag).getId() > 0;
         } catch (ControllerException c) {
             return this.handleWebException(c, resp);
-        } 
-        
+        }
+
         if (loggedIn) {
             return this.handleWebException(new ControllerException("对不起，您已经登录了"), resp);
         }
@@ -290,39 +309,38 @@ public class UserController extends AbstractController{
         if (passwordVo.getAuthCode() == null) {
             return this.handleWebException(new ControllerException("验证码不能为空"), resp);
         }
-        
+
         SessionBo newSession = null;
         UserBo result = null;
         try {
-            newSession = userFacade.recoverPassword(PasswordConverter.fromModel(passwordVo), permissionTag); 
+            newSession = userFacade.recoverPassword(PasswordConverter.fromModel(passwordVo), permissionTag);
             if (newSession != null && newSession.getId() > 0) {
                 result = userFacade.getCurrentUser(newSession, permissionTag);
                 this.openSession(newSession, false, req, resp);
-            }
-            else {
+            } else {
                 throw new ControllerException("找回密码出现错误，创建新Session时失败");
             }
         } catch (ControllerException c) {
             return this.handleWebException(c, resp);
-        }  
+        }
         responseVo = UserConverter.toModel(result);
         return responseVo;
     }
-    
-    
+
     @RequestMapping(value = "/{id}/changePassword", method = RequestMethod.PUT, consumes = "application/json", produces = "application/json")
-    public @ResponseBody JsonResponse changePassword(@PathVariable("id") int id, @RequestBody PasswordVo passwordVo, HttpServletRequest req, HttpServletResponse resp) {
+    public @ResponseBody
+    JsonResponse changePassword(@PathVariable("id") int id, @RequestBody PasswordVo passwordVo, HttpServletRequest req, HttpServletResponse resp) {
         String permissionTag = this.getUrl(req);
         SessionBo authSessionBo = this.getSession(req);
-        
+
         int curId;
         try {
             curId = userFacade.authenticate(authSessionBo, permissionTag).getId();
         } catch (ControllerException c) {
             return this.handleWebException(c, resp);
-        } 
-        
-        boolean loggedIn =  curId > 0;
+        }
+
+        boolean loggedIn = curId > 0;
         if (!loggedIn) {
             return this.handleWebException(new ControllerException("对不起，您尚未登录"), resp);
         }
@@ -336,7 +354,7 @@ public class UserController extends AbstractController{
         if (passwordVo.getNewPassword() == null) {
             return this.handleWebException(new ControllerException("新密码不能为空"), resp);
         }
-        
+
         SessionBo newSession = null;
         try {
             PasswordBo passwordBo = PasswordConverter.fromModel(passwordVo);
@@ -344,86 +362,84 @@ public class UserController extends AbstractController{
             newSession = userFacade.changePassword(passwordBo, permissionTag);
         } catch (ControllerException c) {
             return this.handleWebException(c, resp);
-        } 
-        
+        }
+
         this.openSession(newSession, false, req, resp);
         return new EmptyResponse();
     }
-    
-    
+
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = "application/json")
-    public @ResponseBody JsonResponse getById(@PathVariable("id") int id, HttpServletRequest req, HttpServletResponse resp) {
+    public @ResponseBody
+    JsonResponse getById(@PathVariable("id") int id, HttpServletRequest req, HttpServletResponse resp) {
         UserVo responseVo = null;
-        
+
         String permissionTag = this.getUrl(req);
         SessionBo authSessionBo = this.getSession(req);
-        
+
         UserBo curUser = null;
         try {
             curUser = userFacade.authenticate(authSessionBo, permissionTag);
         } catch (ControllerException c) {
             return this.handleWebException(c, resp);
-        } 
-        
-        
+        }
+
         int curId = curUser.getId();
-        boolean loggedIn =  curId > 0;
+        boolean loggedIn = curId > 0;
         if (!loggedIn) {
             return this.handleWebException(new ControllerException("对不起，您尚未登录"), resp);
         }
-        //user module specific (and maybe partner?)
+        // user module specific (and maybe partner?)
         if (curId != id) {
             return this.handleWebException(new ControllerException("对不起，您没有权限查看其他用户资料"), resp);
         }
-        
+
         UserVo queryUser = new UserVo();
         queryUser.setId(id);
         UserBo responseUser = null;
         try {
-            responseUser = userFacade.queryUserInfo(UserConverter.fromModel(queryUser), curUser, permissionTag); 
+            responseUser = userFacade.queryUserInfo(UserConverter.fromModel(queryUser), curUser, permissionTag);
         } catch (ControllerException c) {
             return this.handleWebException(c, resp);
-        }  
+        }
         responseVo = UserConverter.toModel(responseUser);
         return responseVo;
     }
-    
-    
+
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT, consumes = "application/json", produces = "application/json")
-    public @ResponseBody JsonResponse update(@PathVariable("id") int id, @RequestBody UserVo userVo, HttpServletRequest req, HttpServletResponse resp) {
+    public @ResponseBody
+    JsonResponse update(@PathVariable("id") int id, @RequestBody UserVo userVo, HttpServletRequest req, HttpServletResponse resp) {
         UserVo responseVo = null;
-        
+
         String permissionTag = this.getUrl(req);
         SessionBo authSessionBo = this.getSession(req);
-        
+
         UserBo curUser = null;
         try {
             curUser = userFacade.authenticate(authSessionBo, permissionTag);
         } catch (ControllerException c) {
             return this.handleWebException(c, resp);
-        } 
-        
+        }
+
         int curId = curUser.getId();
-        boolean loggedIn =  curId > 0;
+        boolean loggedIn = curId > 0;
         if (!loggedIn) {
             return this.handleWebException(new ControllerException("对不起，您尚未登录"), resp);
         }
         if (curId != id) {
             return this.handleWebException(new ControllerException("对不起，您只能更新自己的用户信息"), resp);
         }
-        
-        
+
         UserBo targetUser = UserConverter.fromModel(userVo);
         targetUser.setId(id);
-        
+
         UserBo responseUser = null;
         try {
-            responseUser = userFacade.updateUser(targetUser, curUser, permissionTag); 
+            responseUser = userFacade.updateUser(targetUser, curUser, permissionTag);
         } catch (ControllerException c) {
             return this.handleWebException(c, resp);
-        }  
+        }
         responseVo = UserConverter.toModel(responseUser);
         return responseVo;
     }
-    
+
 }
